0bf57ae618232a4db40afdea05c6d725cbb804f2
bensmith <benhsmith13@gmail.com>
2016-06-03 15:15:15 -0600

    Comments out all the stuff that didn’t work, need to get the data in here

--- client/js/components/main/start.jsx
+++ client/js/components/main/start.jsx
@@ -13,32 +13,37 @@ import FullPostNav        from "../post_nav/full_post_nav.jsx";
 
 const select = (state) => {
   return {
-    settings: state.settings
+    settings      : state.settings,
+    assessment    : state.assessment
   };
 };
 
-@connect(select, {}, null, {withRef: true})
+@connect(select, {...AssessmentActions}, null, {withRef: true})
 export default class Start extends React.Component{
+  
+  constructor(){
+    super();
+    this.state = {}
+  }
 
   componentWillMount(){
     var showStart = this.props.settings.enableStart && !AssessmentStore.isStarted();
     if(!this.props.settings.showStart){
-          AssessmentActions.start(this.props.settings.eId, this.props.settings.assessmentId, this.props.settings.externalContextId);
-          context.router.transitionTo("assessment");
+          // this.props.start(this.props.settings.eId, this.props.settings.assessmentId, this.props.settings.externalContextId);
+          // context.router.transitionTo("assessment");
     }
   }
 
   componentDidMount(){
-    super.componentDidMount();
     if(this.state.isLoaded){
       // Trigger action to indicate the assessment was viewed
-      AssessmentActions.assessmentViewed(this.state.settings, this.state.assessment);
+      // AssessmentActions.assessmentViewed(this.props.settings, this.props.assessment);
     }
     CommHandler.sendSize();
   }
 
   getStyles(theme){
-    var minWidth = this.state.settings.assessmentKind.toUpperCase()  == "FORMATIVE" ? "480px" : "635px";
+    var minWidth = this.props.settings.assessmentKind && this.props.settings.assessmentKind.toUpperCase()  == "FORMATIVE" ? "480px" : "635px";
     return {
       progressBar: {
         backgroundColor: theme.progressBarColor,
@@ -48,7 +53,7 @@ export default class Start extends React.Component{
         height: theme.progressBarHeight
       },
       assessment: {
-        padding: this.state.settings.assessmentKind.toUpperCase()  == "FORMATIVE" ? "" : theme.assessmentPadding,
+        padding: this.props.settings.assessmentKind && this.props.settings.assessmentKind.toUpperCase()  == "FORMATIVE" ? "" : theme.assessmentPadding,
         backgroundColor: theme.assessmentBackground,
         minWidth: minWidth
       },
@@ -77,33 +82,33 @@ export default class Start extends React.Component{
   }
 
   render(){
-    var styles = this.getStyles(this.context.theme)
+    var styles = this.getStyles({});
     var content;
     var progressBar;
     var titleBar;
 
     if(this.state.showStart){
         content = <CheckUnderstanding
-          title           = {this.props.settings.assessmentTitle}
-          maxAttempts     = {this.props.settings.allowedAttempts}
-          userAttempts    = {this.props.settings.userAttempts}
-          eid             = {this.props.settings.lisUserId}
-          userId          = {this.props.settings.userId}
-          isLti           = {this.props.settings.isLti}
-          assessmentId    = {this.props.settings.assessmentId}
-          assessmentKind  = {this.props.settings.assessmentKind}
-          ltiRole         = {this.props.settings.ltiRole}
-          externalContextId = {this.props.settings.externalContextId}
-          accountId       = {this.props.settings.accountId}
-          icon            = {this.props.settings.images.QuizIcon_svg}/>;
+          title             = {this.props.settings && this.props.settings.assessmentTitle}
+          maxAttempts       = {this.props.settings && this.props.settings.allowedAttempts}
+          userAttempts      = {this.props.settings && this.props.settings.userAttempts}
+          eid               = {this.props.settings && this.props.settings.lisUserId}
+          userId            = {this.props.settings && this.props.settings.userId}
+          isLti             = {this.props.settings && this.props.settings.isLti}
+          assessmentId      = {this.props.settings && this.props.settings.assessmentId}
+          assessmentKind    = {this.props.settings && this.props.settings.assessmentKind}
+          ltiRole           = {this.props.settings && this.props.settings.ltiRole}
+          externalContextId = {this.props.settings && this.props.settings.externalContextId}
+          accountId         = {this.props.settings && this.props.settings.accountId}
+          icon              = {this.props.settings && this.props.settings.images.QuizIcon_svg}/>;
         progressBar = <div style={styles.progressContainer}>
-                            <ProgressDropdown disabled={true} settings={this.state.settings} questions={this.state.allQuestions} currentQuestion={this.state.currentItemIndex + 1} questionCount={this.state.questionCount} />
+                            <ProgressDropdown disabled={true} settings={this.props.settings} questions={this.state.allQuestions} currentQuestion={this.state.currentItemIndex + 1} questionCount={this.state.questionCount} />
                           </div>;
 
     }
-    var quizType = this.state.settings.assessmentKind.toUpperCase() === "SUMMATIVE" ? "Quiz" : "Show What You Know";
-    var titleBar = this.state.settings.assessmentKind.toUpperCase() === "FORMATIVE" ?  "" : <div style={styles.titleBar}>{this.state.settings ? this.state.settings.assessmentTitle : ""}</div>;
-    progressBar = this.state.settings.assessmentKind.toUpperCase() === "FORMATIVE" ? "" : progressBar;
+    var quizType = this.props.settings.assessmentKind && this.props.settings.assessmentKind.toUpperCase() === "SUMMATIVE" ? "Quiz" : "Show What You Know";
+    var titleBar = this.props.settings.assessmentKind && this.props.settings.assessmentKind.toUpperCase() === "FORMATIVE" ?  "" : <div style={styles.titleBar}>{this.props.settings ? this.props.settings.assessmentTitle : ""}</div>;
+    progressBar = this.props.settings.assessmentKind && this.props.settings.assessmentKind.toUpperCase() === "FORMATIVE" ? "" : progressBar;
 
     return <div className="assessment" style={styles.assessment}>
       {titleBar}
@@ -113,7 +118,9 @@ export default class Start extends React.Component{
           {content}
         </div>
       </div>
-      <FullPostNav/>
+      {
+        //<FullPostNav/>
+      }
     </div>;
   }
 
