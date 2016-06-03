9192dabeceb1247301e10294163739a5866c4fa9
bensmith <benhsmith13@gmail.com>
2016-06-03 14:31:04 -0600

    Updates one of the containers

--- client/js/components/assessment_results/_assessment_result.jsx
+++ client/js/components/assessment_results/_assessment_result.jsx
@@ -1,45 +1,41 @@
 "use strict";
 
-import React                from 'react';
-import AssessmentStore      from "../../stores/assessment";
-import SettingsStore        from "../../stores/settings";
-import AssessmentActions    from "../../actions/assessment";
-import FormativeResult      from "./formative_result.jsx";
-import SummativeResult      from "./summative_result.jsx";
-import ResultStyles         from "./result_styles.js";
-import CommunicationHandler from "../../utils/communication_handler";
+import React                      from 'react';
+import { connect }                from "react-redux";
+import * as AssessmentActions     from "../../actions/assessment";
+import FormativeResult            from "./formative_result.jsx";
+import SummativeResult            from "./summative_result.jsx";
+import ResultStyles               from "./result_styles.js";
+import CommunicationHandler       from "../../utils/communication_handler";
 
+const select = (state) => {
+  return {
+    assessmentResult : state.assessment.assessmentResult(),
+    timeSpent        : state.assessment.timeSpent(),
+    questions        : state.assessment.allQuestions(),
+    outcomes         : state.assessment.outcomes(),
+    assessment       : state.assessment.current(),
+    settings         : state.settings.current()
+  }
+};
+
+@connect(select, {...AssessmentActions}, null, {withRef: true})
 export default class AssessmentResult extends React.Component{
 
   constructor(props, context){
     super(props, context);
-    this._bind("getStyles");
-    this.stores = [AssessmentStore, SettingsStore];
-    this.state = this.getState();
     this.sendLtiOutcome();
     this.sendAnalytics();
   }
 
-  getState(props, context){
-
-    return {
-      assessmentResult : AssessmentStore.assessmentResult(),
-      timeSpent        : AssessmentStore.timeSpent(),
-      questions        : AssessmentStore.allQuestions(),
-      outcomes         : AssessmentStore.outcomes(),
-      settings         : SettingsStore.current(),
-      assessment       : AssessmentStore.current()
-    }
-  }
-
   sendAnalytics(){
     if(this.state.assessmentResult && this.state.assessmentResult.assessment_results_id) {
-      AssessmentActions.assessmentPostAnalytics(this.state.assessmentResult.assessment_results_id, this.state.settings.externalUserId, this.state.settings.externalContextId);
+      this.props.assessmentPostAnalytics(this.state.assessmentResult.assessment_results_id, this.state.settings.externalUserId, this.state.settings.externalContextId);
     }
   }
   sendLtiOutcome(){
     if(this.isSummative() && this.state.assessmentResult.assessment_results_id){
-      AssessmentActions.assessmentPostLtiOutcome(this.state.assessmentResult.assessment_results_id);
+      this.props.assessmentPostLtiOutcome(this.state.assessmentResult.assessment_results_id);
     }
   }
 
