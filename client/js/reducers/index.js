79f230203401f7ec52413dcc64473281d2e93b0f
dittonjs <jditton.atomic@gmail.com>
2016-06-03 13:15:46 -0600

    removes unused components

--- client/js/reducers/index.js
+++ client/js/reducers/index.js
@@ -2,17 +2,12 @@ import { combineReducers }              from 'redux';
 import settings                         from './settings';
 import application                      from './application';
 import messages                         from './messages';
-import adminAccounts                    from './admin/accounts';
-import adminNavigation                  from './admin/navigation';
-import adminUsers                       from './admin/users';
+
 
 const rootReducer = combineReducers({
   settings,
   application,
-  messages,
-  adminAccounts,
-  adminNavigation,
-  adminUsers
+  messages
 });
 
 export default rootReducer;
