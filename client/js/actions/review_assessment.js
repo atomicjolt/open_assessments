c02c503101913d481a83aa9d3ea69108a2d7ef7d
David Spencer <davidjspencer@ufl.edu>
2016-06-03 13:48:40 -0600

    turn review_assessment action to redux

--- client/js/actions/review_assessment.js
+++ client/js/actions/review_assessment.js
@@ -1,25 +1,28 @@
 "use strict";
 
-import Constants   from   "../constants";
-import Api         from   "./api";
-import Dispatcher  from   "../dispatcher";
+import Network  from   "../constants/network";
+import wrapper     from   "../constants/wrapper";
 
-export default {
+const requests = [
+  "REVIEW_ASSESSMENT_LOADED",
+  "REVIEW_RESULT_LOADED"
+];
 
-  loadAssessment(settings){
-    Dispatcher.dispatch({ action: Constants.REVIEW_ASSESSMENT_LOAD_PENDING });
-    var url = settings.srcUrl;
-    if(url.indexOf("?") > -1) {
-      url = url+ "&for_review=1"
-    } else {
-      url = url+ "?for_review=1"
-    }
-    Api.get(Constants.REVIEW_ASSESSMENT_LOADED, url);
-  },
+const actions  = [
+  "REVIEW_ASSESSMENT_LOAD_PENDING",
+  "REVIEW_RESULT_LOAD_PENDING"
+];
 
-  loadAssessmentResult(assessmentId, resultId){
-    Dispatcher.dispatch({ action: Constants.REVIEW_RESULT_LOAD_PENDING });
-    Api.get(Constants.REVIEW_RESULT_LOADED, "/api/assessments/" + assessmentId + "/results/" + resultId);
-  }
+export const Constants = wrapper(requests, actions);
 
-};
+export const loadAssessment = (settings) => ({
+  type: Constants.REVIEW_ASSESSMENT_LOAD_PENDING,
+  method: Network.GET,
+  url: (url.indexOf("?") > -1) ? `${settings.srcUrl}&for_review=` : `${settings.srcUrl}?for_review=1`
+});
+
+export const loadAssessmentResult = (assessmentId, resultId) => ({
+  type: Constants.REVIEW_RESULT_LOAD_PENDING,
+  method: Network.GET,
+  url: `/api/assessments/${assessmentId}/results/${resultId}`
+}
