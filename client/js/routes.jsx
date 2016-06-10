8fa605b506424492b653251aa15149e37c234384
Justin Ball <justinball@gmail.com>
2016-06-09 20:30:31 -0600

    Removes unused routes

--- client/js/routes.jsx
+++ client/js/routes.jsx
@@ -7,10 +7,7 @@ import appHistory         from './history';
 import Index              from './components/index';
 import Assessment         from './components/main/assessment';
 import Start              from './components/main/start';
-import Attempts           from './components/main/attempts';
 import AssessmentResult   from './components/assessment_results/_assessment_result';
-import TeacherReview      from './components/assessment_results/teacher_review';
-import TeacherPreview     from './components/assessment_results/teacher_preview';
 import NotFound           from './components/not_found';
 import About              from './components/main/about';
 
@@ -21,10 +18,7 @@ export default (
       <IndexRoute component={Start} />
       <Route path="assessment" handler={Assessment}/>
       <Route path="assessment-result" handler={AssessmentResult}/>
-      <Route path="review/:assessmentId/:attempdId" handler={TeacherReview} /> //teacher-review
-      <Route path="preview/:assessmentId" handler={TeacherPreview} /> // path="teacher-preview"
       <Route path="about" handler={About}/>
-      <Route path="attempts/:assessmentId/:contextId" handler={Attempts} /> //path="attempts"
       <Route path="*" component={NotFound}/>
     </Route>
   </Router>
