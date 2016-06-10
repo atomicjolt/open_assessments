1041136b41acf0f1a0838dbc3ab2a5eafbd11f6b
Justin Ball <justinball@gmail.com>
2016-06-09 22:11:53 -0600

    Adds rapper to assessment result spec

--- client/js/components/assessment_results/_assessment_result.spec.js
+++ client/js/components/assessment_results/_assessment_result.spec.js
@@ -2,48 +2,30 @@ import React              from 'react';
 import ReactDOM           from 'react-dom';
 import TestUtils          from 'react/lib/ReactTestUtils';
 import AssessmentResult   from './_assessment_result';
-import AssessmentActions  from '../../actions/assessment';
-// import SettingsActions    from '../../actions/settings';
+import Rapper             from '../../../specs_support/rapper';
+import configureStore     from '../../store/configure_store';
 
 describe('assessment result', function() {
-  var srcData;
-  var settings;
+
+  var result;
+
   beforeAll(function(){
     jasmine.getFixtures().fixturesPath = "base/specs_support/fixtures";
-    srcData = readFixtures("qti1/text.xml");
-    settings = {
-      assessmentKind: "formative",
-      srcUrl: "asdf",
-      images: {}
+    const settings = {};
+    const assessment =  readFixtures("qti1/text.xml");
+    var state = {
+      settings,
+      assessment
     };
+    var store = configureStore(state);
+    result = TestUtils.renderIntoDocument(<Rapper childProps={{store, params}} child={AssessmentResult}/>);
 
-  });
-
-  beforeEach(() => {
-
-    jasmine.clock().install();
-    AssessmentActions.loadAssessment(settings, srcData);
-    jasmine.clock().tick();
-    SettingsActions.load(settings);
-    jasmine.clock().tick();
-    jasmine.Ajax.install();
-    AssessmentActions.submitAssessment("100", 0, [{}], "answers", {});
-    jasmine.Ajax.requests.mostRecent().respondWith({
-      "status"        : 200,
-      "contentType"     : "text/plain",
-      "responseText" : JSON.stringify({score: "100", feedback: "you did good", correct_list: [true], confidence_level_list: ["maybe"]})
-    });
-    jasmine.clock().tick(); // Advance the clock to the next tick
-  });
-
-  afterEach(() => {
-    jasmine.clock().uninstall();
-    jasmine.Ajax.uninstall();
   });
 
   it('renders the assessment result', function(){
-    var result = TestUtils.renderIntoDocument(<AssessmentResult />);
-    var content = ReactDOM.findDOMNode(result).textContent;
+    instance = result.refs.original.getWrappedInstance();
+    expect(ReactDOM.findDOMNode(instance)).toBeDefined();
+    var content = ReactDOM.findDOMNode(instance).textContent;
     expect(content.length > 0).toBe(true);
   });
 });
