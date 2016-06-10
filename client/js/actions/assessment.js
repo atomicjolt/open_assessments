ebde6de0069e7579200fa1e12c760c4338f3374f
Justin Ball <justinball@gmail.com>
2016-06-09 20:36:40 -0600

    Adds ASSESSMENT_VIEWED

--- client/js/actions/assessment.js
+++ client/js/actions/assessment.js
@@ -7,7 +7,8 @@ const constants = [];
 const requests = [
   "LOAD_ASSESSMENT",
   "ASSESSMENT_POST_ANALYTICS",
-  "ASSESSMENT_POST_LTI_OUTCOME"
+  "ASSESSMENT_POST_LTI_OUTCOME",
+  "ASSESSMENT_VIEWED"
 ];
 
 export const Constants = wrapper(constants, requests);
@@ -28,4 +29,8 @@ export const assessmentPostAnalytics = (resultsId, userId='', contextId='') => (
 export const assessmentPostLtiOutcome = (resultsId) => ({
   type:Constants.ASSESSMENT_POST_LTI_OUTCOME,
   resultsId,
+});
+
+export const assessmentViewed = () => ({
+  type:Constants.ASSESSMENT_VIEWED
 });
