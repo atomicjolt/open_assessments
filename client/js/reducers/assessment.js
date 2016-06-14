380643eb116a211500939261fe688ed77a818971
Justin Ball <justinball@gmail.com>
2016-06-13 22:32:55 -0600

    Revert to only assessment as payload

--- client/js/reducers/assessment.js
+++ client/js/reducers/assessment.js
@@ -9,7 +9,7 @@ export default (state = initialState, action) => {
   switch(action.type){
 
     case AssessmentConstants.LOAD_ASSESSMENT_DONE:
-      return action.payload.assessment;
+      return action.payload;
       break;
 
     default:
