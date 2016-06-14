c7cabe5ba329fc10156f3e608d8e30fe334d6d90
Justin Ball <justinball@gmail.com>
2016-06-13 23:03:51 -0600

    Adds missing logic for initial load of assessment data from page.

--- client/js/app.jsx
+++ client/js/app.jsx
@@ -7,12 +7,14 @@ import ReactDOM                from 'react-dom';
 import { Provider }            from 'react-redux';
 import Immutable               from 'immutable';
 import _                       from 'lodash';
+import $                       from 'jquery';
 
 import routes                  from './routes';
 import DevTools                from './dev/dev_tools';
 import configureStore          from './store/configure_store';
 import jwt                     from './loaders/jwt';
 import QueryString             from './utils/query_string';
+import { htmlDecodeWithRoot }  from './utils/utils';
 
 // Polyfill es6 promises for IE
 es6Promise.polyfill();
@@ -40,12 +42,27 @@ class Root extends React.Component {
   }
 }
 
-// Build settings from DEFAULT_SETTINGS, the url, and data embedded in the page
-var settings = _.merge(window.DEFAULT_SETTINGS, QueryString.params());
-const el = document.getElementById('assessment_data');
-if(el && el.innerText.length > 0){
-  settings.assessment_data = el.innerText;
+function getAssessmentData(){
+  var data = null;
+  const el = document.getElementById('assessment_data');
+  if(el && el.innerText.length > 0){
+    data = el.innerText;
+  }
+  var result = htmlDecodeWithRoot(data);
+  if(!result){ // Result was empty. Try returning the raw result
+    result = data;
+  }
+  result = _.trim(result);
+  result = result.replace('<![CDATA[', '');
+  result = result.replace('<!--[CDATA[', '');
+  if(result.slice(-3) == ']]>'){
+    result = result.slice(0,-3);
+  }
+  return result;
 }
+
+// Build settings from DEFAULT_SETTINGS, the url, and data embedded in the page
+const settings = _.merge(window.DEFAULT_SETTINGS, QueryString.params(), { assessment_data: getAssessmentData() });
 settings = Immutable.fromJS(settings);
 
 const store = configureStore({settings});
