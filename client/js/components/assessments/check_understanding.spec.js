024edd5c0c13d1264b19393a9cb1afca95f64197
Nick <nick.benoit14@gmail.com>
2016-06-16 10:13:34 -0600

    removed styles and context from check_understanding

--- client/js/components/assessments/check_understanding.spec.js
+++ client/js/components/assessments/check_understanding.spec.js
@@ -1,31 +1,28 @@
 import React              from 'react';
 import ReactDOM           from 'react-dom';
-import { createStore } from 'redux'
+import { createStore }    from 'redux';
 
 import TestUtils          from 'react/lib/ReactTestUtils';
-import CheckUnderstanding from './check_understanding';
+import { CheckUnderstanding } from './check_understanding';
 
 describe('check understanding', function() {
 
   describe('formative start screen', ()=>{
-    let store = createStore(() => ({
-      settings:{
-        title : "Test Title",
-        maxAttempts : 2,
-        userAttempts : 1,
-        eid : "asdf",
-        isLti : true,
-        assessmentId : 24,
-        assessmentKind : "formative",
-        ltiRole : "User",
-        icon : "test.svg",
-        images : {},
-        theme : {}
-      }
-    }));
+    let props = {
+      title : "Test Title",
+      maxAttempts : 2,
+      userAttempts : 1,
+      eid : "asdf",
+      isLti : true,
+      assessmentId : 24,
+      assessmentKind : "formative",
+      ltiRole : "User",
+      icon : "test.svg",
+      images : {},
+      theme : {}
+    };
 
-    var Subject = <CheckUnderstanding store={store} />
-    var result = TestUtils.renderIntoDocument(Subject);
+    var result = TestUtils.renderIntoDocument(<CheckUnderstanding {...props} />);
     it('renders the formative start Screen', function() {
       expect(ReactDOM.findDOMNode(result).textContent).toContain("Test Title");
       expect(ReactDOM.findDOMNode(result).textContent).toContain("Start Quiz");
@@ -33,49 +30,44 @@ describe('check understanding', function() {
   });
 
   describe('show what you know start screen', ()=>{
-    let store = createStore( () => ({
-      settings: {
-        title : "Test Title",
-        maxAttempts : 2,
-        userAttempts : 1,
-        eid : "asdf",
-        isLti : true,
-        assessmentId : 24,
-        assessmentKind : "show_what_you_know",
-        ltiRole : "User",
-        icon : "test.svg",
-        images : {},
-        theme : {}
-      }
-    }))
+    let props = {
+      title : "Test Title",
+      maxAttempts : 2,
+      userAttempts : 1,
+      eid : "asdf",
+      isLti : true,
+      assessmentId : 24,
+      assessmentKind : "show_what_you_know",
+      ltiRole : "User",
+      icon : "test.svg",
+      images : {},
+      theme : {}
+    };
 
-
-    var Subject = <CheckUnderstanding store={store} />
-    var result = TestUtils.renderIntoDocument(Subject);
+    var result = TestUtils.renderIntoDocument(<CheckUnderstanding {...props}/>);
     it('renders the show what you know start screen', function() {
-      expect(ReactDOM.findDOMNode(result).textContent).toContain("Take this pre-test to see what you already know about the concepts");
-      expect(ReactDOM.findDOMNode(result).textContent).toContain("Start Pre-test");
+      const subject = ReactDOM.findDOMNode(result).textContent;
+      expect(subject).toContain("Take this pre-test to see what you already know about the concepts");
+      expect(subject).toContain("Start Pre-test");
     });
   });
 
   describe('summative start screen', ()=>{
-    let store = createStore(() => ({
-      settings:{
-        title : "Test Title",
-        maxAttempts : 2,
-        userAttempts : 0,
-        eid : "asdf",
-        isLti : true,
-        assessmentId : 24,
-        assessmentKind : "summative",
-        ltiRole : "User",
-        icon : "test.svg",
-        images : {},
-        theme : {}
-      }
-    }));
+    let props = {
+      title : "Test Title",
+      maxAttempts : 2,
+      userAttempts : 0,
+      eid : "asdf",
+      isLti : true,
+      assessmentId : 24,
+      assessmentKind : "summative",
+      ltiRole : "User",
+      icon : "test.svg",
+      images : {},
+      theme : {}
+    };
 
-    var result = TestUtils.renderIntoDocument(<CheckUnderstanding store={store} />);
+    var result = TestUtils.renderIntoDocument(<CheckUnderstanding {...props} />);
     it('renders the summative start screen', function() {
       expect(ReactDOM.findDOMNode(result).textContent).toContain("You can take this quiz twice.");
       expect(ReactDOM.findDOMNode(result).textContent).toContain("1of 2");
@@ -83,9 +75,7 @@ describe('check understanding', function() {
     });
 
     it('renders a message if user has hit max attempts', function() {
-      let props = {
-        userAttempts: 2
-      };
+      props.userAttempts = 2;
       result = TestUtils.renderIntoDocument(<CheckUnderstanding {...props} />);
       expect(ReactDOM.findDOMNode(result).textContent).toContain("Oops!You have already taken this quiz the maximum number of times");
     });
