8bf8fe2ce78bd8d545528708246703424a882659
Nick <nick.benoit14@gmail.com>
2016-06-20 09:58:54 -0600

    implement instructions

--- client/js/components/main/start.jsx
+++ client/js/components/main/start.jsx
@@ -22,20 +22,35 @@ export class Start extends React.Component{
 
   instructions(){
     const kind = this.props.kind.get('assessmentKind', '').toUpperCase();
+    let instruction;
 
     switch (kind) {
       case "SUMMATIVE":
         // Get summative instructions
+        // NOTE progress bar
+        instruction = (<div>Summative Quiz</div>);
         break;
       case "FORMATIVE":
         // Get formative instructions
+        // NOTE no progress bar
+        instruction = (<div>Formative Quiz</div>);
         break;
       case "SHOW_WHAT_YOU_KNOW":
         // Get show what you know instructions
+        // NOTE progress bar
+        instruction = (
+          <div>
+            <h2>Show What You Know</h2>
+            <div>Take this pre-test to see what you already know about the concepts in this section.</div>
+            <div>The pre-test does not count toward your grade, but will help you plan where to focus</div>
+            <div>your time and effort as you study. The pre-test is optional.</div>
+          </div>
+        );
         break;
       default:
-        return "";
+        instruction = (<div></div>);
     }
+    return instruction;
   }
 
   render(){
@@ -76,6 +91,7 @@ export class Start extends React.Component{
 // NOTE case 1 - Only start button
 // NOTE case 2 - Instructions and start button
 // NOTE Skip to questions moved to index
+// NOTE Moved check attempts to index
 // NOTE Move send size to index
 // NOTE Moved assessment viewed to assessment
 
