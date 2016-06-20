2b831549055a924870ea90b68cb727b3ca63b911
Nick <nick.benoit14@gmail.com>
2016-06-20 15:09:56 -0600

    refactor check parser undefined

--- client/js/selectors/assessment.js
+++ client/js/selectors/assessment.js
@@ -2,18 +2,18 @@ import * as Qti1Selectors  from "../parsers/qti1/selectors";
 import * as Qti2Assessment from "../parsers/qti2/selectors";
 import * as EdxAssessment  from "../parsers/edX/selectors";
 
-function getParser(metaData){
+function getParser(standard){
   return {
     "QTI1": Qti1Selectors,
     "QTI2": Qti2Assessment,
     "EDX": EdxAssessment
-  }[metaData];
+  }[standard];
 }
 
 function makeSelector(name){
   return (state, props) => {
     var parser = getParser(state.assessment.get('standard'));
-    if(!parser){return parser;} // Handle no assessment loaded
+    if(parser === "undefined"){return;} // Handle no assessment loaded
     var func = parser[name];
     return func(state, props);
   };
