f8f3958fd447b4cf7d5e615b15c490a16e0ca0af
Nick <nick.benoit14@gmail.com>
2016-06-20 11:17:37 -0600

    add immutable to assessment reducer

--- client/js/reducers/assessment.js
+++ client/js/reducers/assessment.js
@@ -1,15 +1,16 @@
 "use strict";
 
+import Immutable from 'immutable';
 import { Constants as AssessmentConstants }  from "../actions/assessment";
 
-const initialState = null;
+const initialState = Immutable.fromJS({});
 
 export default (state = initialState, action) => {
 
   switch(action.type){
 
     case AssessmentConstants.LOAD_ASSESSMENT_DONE:
-      return action.payload;
+      return Immutable.fromJS(action.payload);
       break;
 
     default:
