dfa06d6357d10d2c2ebe03fe820709fa13de33d3
David Owen <dsowen@fugue88.ws>
2016-06-21 11:25:23 -0600

    Renamed parser -> selectors

--- client/js/selectors/assessment.js
+++ client/js/selectors/assessment.js
@@ -1,18 +1,18 @@
 import * as Qti1Selectors  from "../parsers/qti1/selectors";
-import * as Qti2Assessment from "../parsers/qti2/selectors";
-import * as EdxAssessment  from "../parsers/edX/selectors";
+import * as Qti2Selectors  from "../parsers/qti2/selectors";
+import * as EdxSelectors   from "../parsers/edX/selectors";
 
-function getParser(metaData){
+function getSelectors(metaData){
   return {
     "QTI1": Qti1Selectors,
-    "QTI2": Qti2Assessment,
-    "EDX": EdxAssessment
+    "QTI2": Qti2Selectors,
+    "EDX": EdxSelectors
   }[metaData.type];
 }
 
-function makeSelector(name){
+function makeDispatchingSelector(name){
   return (state, props) => {
-    var func = getParser(state.assessmentMetaData)[name];
+    var func = getSelectors(state.assessmentMetaData)[name];
     return func(state, props);
   };
 }
@@ -20,6 +20,6 @@ function makeSelector(name){
 // Selectors that will interact with the assessment data.
 // All of these take state and props as parameters and just
 // wrap a call to the selectors native to the assessment.
-export const questions = makeSelector("questions");
-export const outcomes = makeSelector("outcomes");
-export const questionCount = makeSelector("questionCount");
+export const questions      = makeDispatchingSelector("questions");
+export const outcomes       = makeDispatchingSelector("outcomes");
+export const questionCount  = makeDispatchingSelector("questionCount");
