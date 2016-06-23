b866a3407614b0d83a5f65e240deff470bfbd626
David Owen <dsowen@fugue88.ws>
2016-06-23 09:01:49 -0600

    Fixed warnings, indenting, and other style

--- client/js/components/assessments/item.jsx
+++ client/js/components/assessments/item.jsx
@@ -1,8 +1,9 @@
 "use strict";
 
-import React                  from "react";
-import * as AssessmentActions from "../../actions/assessment";
-import UniversalInput         from "./universal_input";
+import React  from "react";
+
+import * as AssessmentActions  from "../../actions/assessment";
+import UniversalInput          from "./universal_input";
 
 export default class Item extends React.Component{
 
@@ -53,8 +54,7 @@ export default class Item extends React.Component{
     if(complete === true){
       window.onbeforeunload = null;
       AssessmentActions.submitAssessment(this.props.assessment.id, this.props.assessment.assessmentId, this.props.allQuestions, this.props.allStudentAnswers, this.props.settings, this.props.outcomes);
-    }
-    else {
+    } else {
       this.setState({unAnsweredQuestions: complete});
     }
   }
@@ -76,16 +76,18 @@ export default class Item extends React.Component{
 
   getFooterNav(){
     if(this.props.shouldShowFooter){
-      return <div>
-              <button onClick={()=>{this.previousButtonClicked();}}>
-              <i className="glyphicon glyphicon-chevron-left"></i>
-              Previous
-              </button>
-              <button onClick={()=>{this.nextButtonClicked();}}>
-                Next
-                <i className="glyphicon glyphicon-chevron-right"></i>
-              </button>
-            </div>;
+      return (
+        <div>
+          <button onClick={()=>{this.previousButtonClicked();}}>
+            <i className="glyphicon glyphicon-chevron-left"></i>
+            Previous
+          </button>
+          <button onClick={()=>{this.nextButtonClicked();}}>
+            Next
+            <i className="glyphicon glyphicon-chevron-right"></i>
+          </button>
+        </div>
+      );
     }
 
     return "";
@@ -93,7 +95,7 @@ export default class Item extends React.Component{
 
   getWarning(state, questionCount, questionIndex){
     if(state && state.unAnsweredQuestions && state.unAnsweredQuestions.length > 0 && questionIndex + 1 == questionCount){
-      return <div><i className="glyphicon glyphicon-exclamation-sign"></i> You left question(s) {state.unAnsweredQuestions.join()} blank. Use the "Progress" drop-down menu at the top to go back and answer the question(s), then come back and submit.</div>
+      return <div><i className="glyphicon glyphicon-exclamation-sign"></i> You left question(s) {state.unAnsweredQuestions.join()} blank. Use the "Progress" drop-down menu at the top to go back and answer the question(s), then come back and submit.</div>;
     }
 
     return "";
@@ -102,16 +104,17 @@ export default class Item extends React.Component{
   getConfidenceLevels(level){
     if(level){
       var levelMessage = <div><b>How sure are you of your answer? Click below to move forward.</b></div>;
-      return    (<div className="confidence_wrapper">
-                  {levelMessage}
-                  <input type="button" className="btn btn-check-answer" value="Just A Guess" onClick={(e) => { this.confidenceLevelClicked(e, this.props.currentItemIndex) }}/>
-                  <input type="button" className="btn btn-check-answer" value="Pretty Sure" onClick={(e) => { this.confidenceLevelClicked(e, this.props.currentItemIndex) }}/>
-                  <input type="button" className="btn btn-check-answer" value="Very Sure" onClick={(e) => { this.confidenceLevelClicked(e, this.props.currentItemIndex) }}/>
-                </div>
-                );
+      return (
+        <div className="confidence_wrapper">
+          {levelMessage}
+          <input type="button" className="btn btn-check-answer" value="Just A Guess" onClick={(e) => { this.confidenceLevelClicked(e, this.props.currentItemIndex); }}/>
+          <input type="button" className="btn btn-check-answer" value="Pretty Sure" onClick={(e) => { this.confidenceLevelClicked(e, this.props.currentItemIndex); }}/>
+          <input type="button" className="btn btn-check-answer" value="Very Sure" onClick={(e) => { this.confidenceLevelClicked(e, this.props.currentItemIndex); }}/>
+        </div>
+      );
     } /*else {
-      return <div className="lower_level"><input type="button" className="btn btn-check-answer" value="Check Answer" onClick={() => { AssessmentActions.checkAnswer()}}/></div>
-    }*/
+         return <div className="lower_level"><input type="button" className="btn btn-check-answer" value="Check Answer" onClick={() => { AssessmentActions.checkAnswer()}}/></div>
+         }*/
   }
 
   getNavigationButtons() {
@@ -119,26 +122,30 @@ export default class Item extends React.Component{
       return "";
     }
 
-    return <div className="confidence_wrapper">
-      {this.getPreviousButton()}
-      {this.getNextButton()}
-    </div>
+    return (
+      <div className="confidence_wrapper">
+        {this.getPreviousButton()}
+        {this.getNextButton()}
+      </div>
+    );
   }
 
   getNextButton() {
     var disabled = (this.props.currentItemIndex == this.props.questionCount - 1) ? "disabled" : "";
     return (
-        <button className={"btn btn-next-item " + disabled} onClick={(e) => { this.nextButtonClicked(e); }}>
-          <span>Next</span> <i className="glyphicon glyphicon-chevron-right"></i>
-        </button>);
+      <button className={"btn btn-next-item " + disabled} onClick={(e) => { this.nextButtonClicked(e); }}>
+        <span>Next</span> <i className="glyphicon glyphicon-chevron-right"></i>
+      </button>
+    );
   }
 
   getPreviousButton() {
     var prevButtonClassName = "btn btn-prev-item " + ((this.props.currentItemIndex > 0) ? "" : "disabled");
     return (
-        <button className={prevButtonClassName} onClick={(e) => { this.previousButtonClicked(e); }}>
-          <i className="glyphicon glyphicon-chevron-left"></i><span>Previous</span>
-        </button>);
+      <button className={prevButtonClassName} onClick={(e) => { this.previousButtonClicked(e); }}>
+        <i className="glyphicon glyphicon-chevron-left"></i><span>Previous</span>
+      </button>
+    );
   }
 
 
@@ -147,18 +154,16 @@ export default class Item extends React.Component{
 
     if(index == -1){
       result = <div className="check_answer_result">
-                <p></p>
-              </div>;
-    }
-    else if(index == 0){
-    result = <div className="check_answer_result">
-                <p>Incorrect</p>
-              </div>;
-    }
-    else {
-      result =  <div className="check_answer_result">
-                  <p>Correct</p>
-                </div>;
+        <p></p>
+      </div>;
+    } else if(index == 0){
+      result = <div className="check_answer_result">
+        <p>Incorrect</p>
+      </div>;
+    } else {
+      result = <div className="check_answer_result">
+        <p>Correct</p>
+      </div>;
     }
 
     return result;
@@ -170,7 +175,7 @@ export default class Item extends React.Component{
     var result = this.getResult(this.props.messageIndex);
     var must_answer_message = this.state && this.state.showMessage ? <div>You must select an answer before continuing.</div> : "";
     var confidenceButtons = this.getConfidenceLevels(this.props.confidenceLevels);
-    var submitButton = (this.props.currentItemIndex == this.props.questionCount - 1) ? <button className="btn btn-check-answer" onClick={(e)=>{this.submitButtonClicked(e)}}>Submit</button> : "";
+    var submitButton = (this.props.currentItemIndex == this.props.questionCount - 1) ? <button className="btn btn-check-answer" onClick={(e) => { this.submitButtonClicked(e); }}>Submit</button> : "";
     var footer = this.getFooterNav();
     var navigationDiv = this.getNavigationButtons();
 
@@ -178,7 +183,7 @@ export default class Item extends React.Component{
     var counter = "";
 
     if(this.props.shouldShowCenter){
-      counter = <span className="counter">{this.props.currentItemIndex + 1} of {this.props.questionCount}</span>
+      counter = <span className="counter">{this.props.currentItemIndex + 1} of {this.props.questionCount}</span>;
     }
     var formativeHeader = "";
     // if(this.props.settings.assessmentKind.toUpperCase() == "FORMATIVE"){
@@ -196,9 +201,7 @@ export default class Item extends React.Component{
     //       </div>
     // }
 
-    var submitButtonDiv =  <div>
-                          {submitButton}
-                        </div>;
+    var submitButtonDiv = <div>{submitButton}</div>;
 
     // if(this.props.settings.assessmentKind.toUpperCase() == "FORMATIVE"){
     //   submitButtonDiv = ""
@@ -206,19 +209,16 @@ export default class Item extends React.Component{
 
     var questionDirections = "";
     if(this.props.question.question_type == "multiple_answers_question"){
-      questionDirections =
-      <div>Choose <b>ALL</b> that apply.</div>
-    }
-    else {
-      questionDirections =
-      <div>Choose the <b>BEST</b> answer.</div>
+      questionDirections = <div>Choose <b>ALL</b> that apply.</div>;
+    } else {
+      questionDirections = <div>Choose the <b>BEST</b> answer.</div>;
     }
 
     return (
       <div className="assessment_container">
         <div className="question">
           <div className="header">
-                {counter}
+            {counter}
             <p>{this.props.question.title}</p>
           </div>
           <div>
@@ -230,8 +230,8 @@ export default class Item extends React.Component{
                     {questionDirections}
                     <div
                       dangerouslySetInnerHTML={{
-                    __html: this.props.question.material
-                    }}>
+                        __html: this.props.question.material
+                      }}>
                     </div>
                   </div>
                   <UniversalInput item={this.props.question} isResult={false}/>
