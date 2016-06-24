9e03737907f44c21549e39514342740d047da0bd
Nick <nick.benoit14@gmail.com>
2016-06-24 09:32:41 -0600

    added progress spec

---
+++ client/js/reducers/progress.spec.js
@@ -0,0 +1,80 @@
+import Immutable                            from "immutable";
+
+import { Constants as AssessmentConstants } from "../actions/assessment_progress";
+import { parse }                            from '../parsers/assessment';
+import progress                             from "./progress";
+
+describe('assessment reducer', () => {
+
+  const settings = Immutable.fromJS({});
+  var initialState;
+  var parsedAssessment;
+
+
+  describe("initial reducer state", () => {
+    it("returns empty state", () => {
+      const state = progress(initialState, {});
+      expect(state.toJS()).toEqual({
+        isSubmitted: false,
+        isStarted: false,
+        currentItemIndex: 0,
+        selectedAnswerId: '',
+        answerMessageIndex: [],
+        responses: [],
+        startedAt: 0,
+        finishedAt: 0,
+        assessmentResult:null
+      });
+    });
+  });
+
+
+  describe("next question", () => {
+    const action = {
+      type: AssessmentConstants.ASSESSMENT_NEXT_QUESTION,
+    };
+
+    it("increments currentItemIndex", () => {
+      var state = progress(undefined, action);
+      expect(state.get('currentItemIndex')).toEqual(1);
+    });
+  });
+
+  describe("previous question", () => {
+    const action = {
+      type: AssessmentConstants.ASSESSMENT_PREVIOUS_QUESTION,
+    };
+    let initialState = Immutable.fromJS({currentItemIndex: 5});
+
+    it("decrements currentItemIndex", () => {
+      const state = progress(initialState, action);
+      expect(state.get('currentItemIndex')).toEqual(4);
+    });
+  });
+
+
+  describe("assessment viewed", () => {
+    const action = {
+      type: AssessmentConstants.ASSESSMENT_VIEWED,
+    };
+
+    it("sets started at time", () => {
+      const state = progress(undefined, action);
+      expect(state.get('startedAt')).not.toEqual(0);
+    });
+  });
+
+  describe("answer selected", () => {
+    const action = {
+      type: AssessmentConstants.ANSWER_SELECTED,
+      questionIndex:0,
+      answerId:1
+    };
+
+    it("adds answerId to respones[index]", () => {
+      const state = progress(undefined, action);
+      debugger;
+      expect(state.getIn(['responses', '0'])).toEqual(1);
+    });
+  });
+});
