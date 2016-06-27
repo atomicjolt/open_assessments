f173bd07b7eb70b04f95b70ed0274c3e2ffd689d
David Owen <dsowen@fugue88.ws>
2016-06-27 09:23:00 -0600

    Add detection for MIT's CLIx format, along with some sample data for testing.

--- client/js/parsers/assessment.js
+++ client/js/parsers/assessment.js
@@ -1,45 +1,49 @@
 import $  from 'jquery';
 
+import CLIx  from './clix/parser';
+//import EdX  from './edX/parser'; // For now we don't need edX
 import Qti1  from './qti1/parser';
 import Qti2  from './qti2/parser';
-//import EdX  from './edX/parser'; // For now we don't need edX
 
 export const AssessmentFormats = {
+  CLIx: "CLIx",
+  EdX: "EDX",
   Qti1: "QTI1",
-  Qti2: "QTI2",
-  EdX: "EDX"
+  Qti2: "QTI2"
 };
 
 // Determines what type of assessment has been passed and parses it correctly,
 // whatever type it is.
 export const parse = (settings, data) => {
 
-  const xml    = $($.parseXML(data));
-  const xmlns  = xml.find('>').attr('xmlns');
+  if(data.startsWith("[")) {
 
-  if(xmlns.startsWith("http://www.imsglobal.org/xsd/imsqti_v2p")) {
-    return Qti2.parse(settings.get("assessmentId"), xml);
-  }
+    // Looks like JSON.  We only have one JSON format.
+    return CLIx.parse(data);
 
-  const assessmentXml   = xml.find('assessment').addBack('assessment');
-  const questestinterop = xml.find('questestinterop').addBack('questestinterop'); // <questestinterop xmlns="http://www.imsglobal.org/xsd/ims_qtiasiv1p2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/ims_qtiasiv1p2 http://www.imsglobal.org/xsd/ims_qtiasiv1p2p1.xsd">
-  const sequential      = xml.find('sequential').addBack('sequential');
+  } else if(data.startsWith("<?xml")) {
 
-  if(assessmentXml.length > 0 || questestinterop.length > 0){
-    return Qti1.parse(settings.get("assessmentId"), assessmentXml, xml);
-  } else if(sequential.length > 0){
-    return EdX.parse(settings, sequential);
+    // Looks like XML.  Detect based on default namespace or parsed structure.
+    const xml    = $($.parseXML(data));
+    const xmlns  = xml.find('>').attr('xmlns');
+
+    if(xmlns.startsWith("http://www.imsglobal.org/xsd/imsqti_v2p")) {
+      return Qti2.parse(settings.get("assessmentId"), xml);
+    }
+
+    // TODO: It would be nice to detect /all/ the XML formats based on
+    // namespace.
+
+    const assessmentXml   = xml.find('assessment').addBack('assessment');
+    const questestinterop = xml.find('questestinterop').addBack('questestinterop'); // <questestinterop xmlns="http://www.imsglobal.org/xsd/ims_qtiasiv1p2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/ims_qtiasiv1p2 http://www.imsglobal.org/xsd/ims_qtiasiv1p2p1.xsd">
+    const sequential      = xml.find('sequential').addBack('sequential');
+
+    if(assessmentXml.length > 0 || questestinterop.length > 0){
+      return Qti1.parse(settings.get("assessmentId"), assessmentXml, xml);
+    } else if(sequential.length > 0){
+      return EdX.parse(settings, sequential);
+    }
   }
 
   throw "Open Assessments could not find valid QTI or EdX XML. Nothing will be rendered. Please verify that your XML meets one of these standards.";
 };
-
-
-
-
-// TODO have to figure out how to handle assessment parsing. We could just
-// Store the raw data in the state and let the selectors figure out the real details
-// by parsing the data on demand.
-// OR we can let the load_assessment middlware use whatever parser it needs to use and then store
-// the resulting object in the state in that form - even if the assessment objects are dramatically different
-// Then we leave it up to the selectors to pull the data out as needed.
