e4eb96b13c9eafa9a256196388bbf45cce4d0e12
Nick <nick.benoit14@gmail.com>
2016-06-30 10:03:53 -0600

    remove unused code

--- client/js/components/common/radio_button.jsx
+++ client/js/components/common/radio_button.jsx
@@ -47,31 +47,6 @@ export default class RadioButton extends React.Component{
     return checked;
   }
 
-  getFeedbackImage(){
-    var content;
-
-    if(this.props.gradeState === CORRECT){
-      content = (
-        <div className="c-feedback  c-feedback--correct">
-          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
-            <path d="M24 4C12.95 4 4 12.95 4 24c0 11.04 8.95 20 20 20 11.04 0 20-8.96 20-20 0-11.05-8.96-20-20-20zm-4 30L10 24l2.83-2.83L20 28.34l15.17-15.17L38 16 20 34z"/>
-          </svg>
-          <span>correct</span>
-        </div>
-      );
-    } else if(this.props.gradeState === INCORRECT) {
-      content = (
-        <div className="c-feedback  c-feedback--incorrect">
-          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
-              <path d="M24 4c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm10 27.17l-2.83 2.83-7.17-7.17-7.17 7.17-2.83-2.83 7.17-7.17-7.17-7.17 2.83-2.83 7.17 7.17 7.17-7.17 2.83 2.83-7.17 7.17 7.17 7.17z"/>
-          </svg>
-          <span>incorrect</span>
-        </div>
-      );
-    }
-
-    return content;
-  }
 
   getFeedback(){
     var content;
@@ -88,7 +63,6 @@ export default class RadioButton extends React.Component{
 
   render() {
     var containerStyle;
-    var feedbackImage = this.getFeedbackImage();
     var feedback = this.getFeedback();
 
     if(this.props.checked === true){containerStyle = "is-clicked";}
