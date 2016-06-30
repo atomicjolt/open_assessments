d576615854cac3fd17420d66c1dc9b6704ec7eb1
David Owen <dsowen@fugue88.ws>
2016-06-30 13:20:48 -0600

    Added spec

---
+++ client/js/parsers/clix/parser.spec.js
@@ -0,0 +1,31 @@
+import Immutable  from "immutable";
+import $          from "jquery";
+
+import { AssessmentFormats, parse }  from "../assessment";
+import Parser                        from "./parser";
+
+describe("CLIx assessment parser", () => {
+
+  beforeAll(() => {
+    jasmine.getFixtures().fixturesPath = "base/specs_support/fixtures";
+  });
+
+  describe("parse", () => {
+
+    it("parses example assessment JSON from CLIx into an object", () => {
+      const data        = readFixtures("clix/assessment.json");
+      const settings    = Immutable.fromJS({ assessmentId: 1 });
+      const assessment  = parse(settings, data);
+
+      expect(assessment).toBeDefined();
+      expect(assessment.id).toEqual(1);
+      expect(assessment.title).toBeUndefined();
+      expect(assessment.standard).toEqual(AssessmentFormats.CLIx);
+      expect(assessment.items.length).toEqual(3);
+      expect(assessment.items[0].title).toEqual("Question 2");
+      expect(assessment.items[1].title).toEqual("Question 3");
+      expect(assessment.items[2].title).toEqual("Social Introductions Role Play");
+    });
+
+  });
+});
