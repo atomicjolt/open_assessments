57ed7188411e20134b8d381cbd4e7a90c90354e4
Nick <nick.benoit14@gmail.com>
2016-07-01 11:21:33 -0600

    documented nav

--- client/js/components/assessments/assessment_nav.jsx
+++ client/js/components/assessments/assessment_nav.jsx
@@ -11,16 +11,33 @@ export const NAV_BUTTON_MODES = {
 export default class AssessmentNav extends React.Component{
 
   static propTypes = {
+
+    // Function to be called when next button is clicked
     nextQuestions: React.PropTypes.func.isRequired,
+
+    // Function to be called when previous button is clicked
     previousQuestions: React.PropTypes.func.isRequired,
+
+    // Function to be called when submit button is clicked
     submitAssessment: React.PropTypes.func.isRequired,
+
+    // Function to be called when check answer button is clicked
     checkAnswers: React.PropTypes.func.isRequired,
 
+    // Whether or not the next set of questions is available to the user
     nextUnlocked: React.PropTypes.bool.isRequired,
+
+    // Whether or not the previous set of questions is available to the user
     previousUnlocked: React.PropTypes.bool.isRequired,
+
+    // Whether or not the check answer button is available to the user
     checkAnswerUnlocked: React.PropTypes.bool.isRequired,
+
+    // Whether or not the submit button is available to the user
     submitButtonUnlocked: React.PropTypes.bool.isRequired,
 
+    // The button configuration to be displayed. Should be one of
+    // NAV_BUTTON_MODES.
     buttonMode: React.PropTypes.string.isRequired
   };
 
@@ -76,10 +93,10 @@ export default class AssessmentNav extends React.Component{
     // Two button mode always renders previous button as left button if enabled
     if(buttonMap.previous === true){buttons.push(this.getPreviousButton());}
 
-    /*
-     * Two button mode selects between next, submit, and check answer buttons for
-     * the right button.
-     */
+
+    // Two button mode selects between next, submit, and check answer buttons for
+    // the right button.
+
     if(buttonMap.submit === true){
       buttons.push(this.getSubmitButton());
     } else if(buttonMap.next === true) {
