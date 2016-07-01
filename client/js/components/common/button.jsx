0afadfe20fa120fb2e050bc158da043ac73fdcb5
Nick <nick.benoit14@gmail.com>
2016-07-01 10:28:26 -0600

    added button class

---
+++ client/js/components/common/button.jsx
@@ -0,0 +1,23 @@
+"use strict";
+
+import React from "react";
+
+export default class Button extends React.Component{
+
+  static propTypes = {
+    buttonType: React.PropTypes.string.isRequired,
+    onClick: React.PropTypes.func.isRequired,
+    buttonText: React.PropTypes.string
+  };
+
+  render(){
+    return (
+      <a
+        className={`c-btn c-btn--${this.props.buttonType}`}
+        onClick={(e) => {this.props.onClick(e);}}>
+        <span>{this.props.buttonType}</span>
+        {this.props.children}
+      </a>
+    );
+  }
+  };
