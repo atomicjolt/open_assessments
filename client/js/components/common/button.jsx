02798a10bc77afea0e934a5f39f395dca113f632
Nick <nick.benoit14@gmail.com>
2016-07-01 10:47:43 -0600

    documented props

--- client/js/components/common/button.jsx
+++ client/js/components/common/button.jsx
@@ -5,8 +5,15 @@ import React from "react";
 export default class Button extends React.Component{
 
   static propTypes = {
+
+    // The css class modifier the button should use.
+    // Will construct css class string: c-btn c-btn--{buttonType}
     buttonType: React.PropTypes.string.isRequired,
+
+    // Function to be called on button click
     onClick: React.PropTypes.func.isRequired,
+
+    // Text to be displayed by button
     buttonText: React.PropTypes.string
   };
 
