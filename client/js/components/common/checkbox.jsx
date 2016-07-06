ba8e3cdd26ae855dd4f1b29c2fd57b6f804c8602
Nick <nick.benoit14@gmail.com>
2016-07-06 12:42:15 -0600

    cleanup checkbox

--- client/js/components/common/checkbox.jsx
+++ client/js/components/common/checkbox.jsx
@@ -18,39 +18,23 @@ export default class CheckBox extends React.Component{
   }
 
   checkedStatus(){
-    var checked = null;
-    var optionFlag = null;
-    if( this.props.checked === true ) {
-      checked = "true";
-    } else if ( this.props.checked === false ){
-      checked = false;
-    } else if ( !this.props.isDisabled ) {
-      checked = (this.props.studentAnswers && this.props.studentAnswers.indexOf(this.props.item.id) > -1) ? "true" : null;
-    }
-    return checked;
+    return this.props.checked === true;
   }
 
-  optionFlagStatus(){
-    if(this.props.showAsCorrect){
-      var label = "Correct Answer that was ";
-      var optionFlag;
-      label += this.checkedStatus() ? "chosen" : "not chosen";
-      optionFlag = <div className="correctIndicator" aria-label={label} style={styles.checkStyleCorrect}>&#10003;</div>;
-    } else if (this.props.showAsCorrect === false && this.checkedStatus()){
-      optionFlag = <div className="wrongIndicator" aria-label="Wrong answer that was chosen" style={styles.checkStyleWrong}>&#10008;</div>;
-    }
-    return optionFlag;
-  }
 
   render(){
-
     return (
       <div>
-        {this.optionFlagStatus()}
+        {/*this.optionFlagStatus() TODO feedback */}
         <div className="btn btn-block btn-question" style={styles.btnQuestion}>
           <label>
-            <input type="checkbox" defaultChecked={this.checkedStatus()} disabled={this.props.isDisabled} name={this.props.name} onClick={()=>{ this.answerSelected(); }}/>
-            <span style={styles.span}>{this.props.item.material}</span>
+            <input
+              type="checkbox"
+              defaultChecked={this.checkedStatus()}
+              disabled={this.props.isDisabled}
+              name={this.props.name}
+              onClick={()=>{ this.answerSelected(); }}/>
+            <span>{this.props.item.material}</span>
           </label>
         </div>
       </div>
