78df9107747e8a878efb96aa347e44c44f698ca7
David Owen <dsowen@fugue88.ws>
2016-07-06 11:06:38 -0600

    Updated parser for new CLIx format

--- client/js/parsers/clix/parser.js
+++ client/js/parsers/clix/parser.js
@@ -7,8 +7,7 @@ import Qti2Parser             from "../qti2/parser.js";
 export default class Parser {
 
   static parse(assessmentId, json) {
-    var jsonItems = JSON.parse(json);
-    var items = jsonItems.map((j) => {
+    var items = json.data.map((j) => {
       var parsed = Qti2Parser.parseItem($($.parseXML(j.qti)).find(">")[0]);
       return Object.assign(parsed, {
         json: j
