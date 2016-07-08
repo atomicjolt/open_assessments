76b42fd4b16053e55970247a7c92c212025c6ce4
Nick <nick.benoit14@gmail.com>
2016-07-08 15:14:34 -0600

    update feedback parser

--- client/js/parsers/clix/parser.js
+++ client/js/parsers/clix/parser.js
@@ -23,12 +23,15 @@ export default class Parser {
 };
 
 export function parseFeedback(feedbackXml){
-  try{
+  if(feedbackXml.startsWith('<?xml')){
     var xml = $.parseXML(feedbackXml);
     var $xml = $(xml);
     var feedback = $xml.find('modalFeedback');
     return feedback.html();
-  } catch(e) {
-    // If we can't parse the feedback, then return undefined
+  } else if(feedbackXML === "No feedback available."){
+    // Don't return any feedback when no feedback is available
+  } else {
+    console.error("We cannot recognize feedback from server");
   }
+
 }
