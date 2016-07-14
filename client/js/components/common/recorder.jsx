c40f1a92c9985ae5d505f0de75d362a7549ade81
Nick <nick.benoit14@gmail.com>
2016-07-14 13:51:24 -0600

    add navigator.getUserMedia polyfill

--- client/js/components/common/recorder.jsx
+++ client/js/components/common/recorder.jsx
@@ -12,36 +12,56 @@ export default class Recorder extends React.Component{
     this.state = {};
   }
 
+  gUMPolyfill(constraints) {
+    // First get ahold of getUserMedia, if present
+    var getUserMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia);
+
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if(!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
+
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise(function(resolve, reject) {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  }
+
   startRecorder(){
     var _this = this;
     if(this.state.recorder && !this.state.recorder.recording){
       this.state.recorder.record();
     } else {
-      navigator.getUserMedia = (
-          navigator.getUserMedia ||
-          navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia ||
-          navigator.webkitGetUserMedia
-         );
-      if (navigator.getUserMedia) {
-        navigator.getUserMedia({ audio: true, video: false },
-          function(stream) {
-            window.AudioContext = window.AudioContext || window.webkitAudioContext;
-            var context = new AudioContext(); //TODO only one audio context
-            var input = context.createMediaStreamSource(stream);
-            var recorder = new Record(input);
-            _this.setState({recorder});
-            recorder.record();
-          },
-          function(err) {
-            console.log("The following error occurred: " + err.name);
-          }
-       );
-      } else {
-        console.log("getUserMedia not supported");
-      }
-    }
 
+
+      // Older browsers might not implement mediaDevices at all, so we set an empty object first
+      if(navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+      }
+
+      // Some browsers partially implement mediaDevices. We can't just assign an object
+      // with getUserMedia as it would overwrite existing properties.
+      // Here, we will just add the getUserMedia property if it's missing.
+      if(navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = this.gUMPolyfill;
+      }
+
+      navigator.mediaDevices.getUserMedia({audio:true})
+      .then(function(stream) {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        var context = new AudioContext();
+        var input = context.createMediaStreamSource(stream);
+        var recorder = new Record(input);
+        _this.setState({recorder});
+        recorder.record();
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+    }
   }
 
   stopRecorder(){
