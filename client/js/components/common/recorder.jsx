f88234e8548bc396ab884d10ae9dd870f750f803
Nick <nick.benoit14@gmail.com>
2016-07-15 13:52:11 -0600

    clean up recorder.jsx

--- client/js/components/common/recorder.jsx
+++ client/js/components/common/recorder.jsx
@@ -4,22 +4,32 @@ import Record    from 'recorderjs';
 export default class Recorder extends React.Component{
 
   static propTypes = {
-    //TODO document
+    // Whether the recorder should start recording or stop recording.
+    // Should be one of 'start' or 'stop'
+    command: React.PropTypes.string.isRequired,
+
+    // function called when recorder stops. Should have signature function(blob){}
+    onStop: React.PropTypes.func.isRequired,
   };
 
   constructor(){
     super();
-    this.state = {};
+    this.state = {
+      command:'stop'
+    };
   }
 
+  /**
+   * Polyfill for browswers that don't support navigator.mediaDevices.getUserMedia
+   */
   gUMPolyfill(constraints) {
     // First get ahold of getUserMedia, if present
     var getUserMedia = (navigator.getUserMedia ||
         navigator.webkitGetUserMedia ||
         navigator.mozGetUserMedia);
 
-    // Some browsers just don't implement it - return a rejected promise with an error
-    // to keep a consistent interface
+    // Some browsers just don't implement it - return a rejected promise with
+    // an error to keep a consistent interface
     if(!getUserMedia) {
       return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
     }
@@ -36,14 +46,14 @@ export default class Recorder extends React.Component{
       this.state.recorder.record();
     } else {
 
-
-      // Older browsers might not implement mediaDevices at all, so we set an empty object first
+      // Older browsers might not implement mediaDevices at all, so we set
+      // an empty object first
       if(navigator.mediaDevices === undefined) {
         navigator.mediaDevices = {};
       }
 
-      // Some browsers partially implement mediaDevices. We can't just assign an object
-      // with getUserMedia as it would overwrite existing properties.
+      // Some browsers partially implement mediaDevices. We can't just assign
+      // an object with getUserMedia as it would overwrite existing properties.
       // Here, we will just add the getUserMedia property if it's missing.
       if(navigator.mediaDevices.getUserMedia === undefined) {
         navigator.mediaDevices.getUserMedia = this.gUMPolyfill;
