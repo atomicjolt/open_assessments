3c0954cb80710ecf9402304459c73d8cf3bd3e9e
Nick <nick.benoit14@gmail.com>
2016-07-22 14:14:52 -0600

    fixed url param setting string bug

--- client/js/reducers/settings.js
+++ client/js/reducers/settings.js
@@ -5,5 +5,14 @@ export default (state = {}, action) => {
 };
 
 export function getInitialSettings(){
-  return _.merge({}, ...arguments); // Add default settings that can be overriden by values in serverSettings
+  var settings = _.merge({}, ...arguments); // Add default settings that can be overriden by values in serverSettings
+
+  // These player settings should be numbers. Change them once here and be done with it.
+  var integerSettings = [ "user_id", "max_attempts", "questions_per_section", "questions_per_page"];
+  _.each(integerSettings, (setting) => {
+    if(settings[setting]){settings[setting] = parseInt(settings[setting]);}
+  });
+  debugger;
+
+  return settings;
 };
