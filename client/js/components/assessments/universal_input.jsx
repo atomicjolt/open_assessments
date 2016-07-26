c79a9d6e2c7c6552a39a876d220111368e376d9f
Nick <nick.benoit14@gmail.com>
2016-07-26 08:58:33 -0600

    wrap every two answers in <ul>

--- client/js/components/assessments/universal_input.jsx
+++ client/js/components/assessments/universal_input.jsx
@@ -77,26 +77,57 @@ export default class UniversalInput extends React.Component{
       case "edx_multiple_choice":
       case "multiple_choice_question":
       case "true_false_question":
-        answerInputs = item.answers.map((answer) => {
-          var selectRadio = _.curryRight(props.selectAnswer);
-          var id = item.id + "_" + answer.id;
-          var gradeState = this.getGradeState(answer.id, props.questionResult);
-          var feedback = this.getFeedback(answer.id, props.questionResult);
+        answerInputs = item.answers.map((answer, i, arr) => {
+          if(!(i % 2)){
+            var row = item.answers.slice(i, i+2);
+            return (
+              <ul className="o-grid">
+                {row.map((answer) => {
+                  var selectRadio = _.curryRight(props.selectAnswer);
+                  var id = item.id + "_" + answer.id;
+                  var gradeState = this.getGradeState(answer.id, props.questionResult);
+                  var feedback = this.getFeedback(answer.id, props.questionResult);
 
-          return (
-            <RadioButton
-                isDisabled={props.isResult}
-                key={id}
-                id={id}
-                item={answer}
-                isHtml={item.isHtml}
-                name="answer-radio"
-                checked={this.wasSelected(answer.id)}
-                gradeState={gradeState}
-                feedback={feedback}
-                selectAnswer={selectRadio(true)}/>
-          );
-        });
+                  return (
+                    <RadioButton
+                        isDisabled={props.isResult}
+                        key={id}
+                        id={id}
+                        item={answer}
+                        isHtml={item.isHtml}
+                        name="answer-radio"
+                        checked={this.wasSelected(answer.id)}
+                        gradeState={gradeState}
+                        feedback={feedback}
+                        selectAnswer={selectRadio(true)}/>
+                  );
+                })}
+              </ul>
+            );
+          }
+        })
+
+
+        // answerInputs = item.answers.map((answer) => {
+        //   var selectRadio = _.curryRight(props.selectAnswer);
+        //   var id = item.id + "_" + answer.id;
+        //   var gradeState = this.getGradeState(answer.id, props.questionResult);
+        //   var feedback = this.getFeedback(answer.id, props.questionResult);
+        //
+        //   return (
+        //     <RadioButton
+        //         isDisabled={props.isResult}
+        //         key={id}
+        //         id={id}
+        //         item={answer}
+        //         isHtml={item.isHtml}
+        //         name="answer-radio"
+        //         checked={this.wasSelected(answer.id)}
+        //         gradeState={gradeState}
+        //         feedback={feedback}
+        //         selectAnswer={selectRadio(true)}/>
+        //   );
+        // });
         break;
       case "edx_dropdown":
         answerInputs = item.answers.map((answer) => {
