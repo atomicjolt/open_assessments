1500f1a111732d709186aa88672c66a9e7940e89
David Owen <dsowen@fugue88.ws>
2016-08-02 13:32:24 -0600

    Fix: initial value should be an array

--- client/js/reducers/assessment_results.js
+++ client/js/reducers/assessment_results.js
@@ -5,7 +5,7 @@ import Immutable  from 'immutable';
 import { Constants }  from '../actions/assessment_progress';
 
 const initialState = Immutable.fromJS({
-  questionResults: {}
+  questionResults: []
 });
 
 export default (state = initialState, action) => {
