aabf76e25cdb87e460817e191823cb9467df23b3
David Owen <dsowen@fugue88.ws>
2016-08-02 15:08:55 -0600

    Have the CLIx selector delegate to the QTI2 selector, instead of copying its code.

--- client/js/parsers/clix/selectors.js
+++ client/js/parsers/clix/selectors.js
@@ -1,7 +1,8 @@
 import $                   from "jquery";
 import { createSelector }  from "reselect";
 
-import { transformItem }  from "./clix";
+import { questionResults as qtiQuestionResults }  from "../qti2/selectors";
+import { transformItem }                          from "./clix";
 
 
 export function questions(state, props) {
@@ -30,26 +31,5 @@ export function isCheckingAnswer(state, props){
 }
 
 export function questionResults(state, props) {
-
-  // TODO Currently we are setting the same response for all userInput.
-  // When we have an example of multi answer feedback we should figure out
-  // how to assign feedback to each answer.
-  const questionIndexes = _.range(
-    state.assessmentProgress.get('currentItemIndex'),
-    state.assessmentProgress.get('currentItemIndex') + state.settings.questions_per_page
-  );
-
-  let questionResponses = {};
-
-  _.each(questionIndexes, (index) => {
-    const response = state.assessmentResults.getIn(['questionResults', index, 0]);
-    if(response) {
-      questionResponses[index] = {};
-      questionResponses[index].correct = response.correct;
-      questionResponses[index].answerIds = response.userInput;
-      questionResponses[index].feedback = response.feedback;
-    }
-  });
-
-  return questionResponses;
+  return qtiQuestionResults(state, props);
 }
