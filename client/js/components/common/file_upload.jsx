e683ed0119e46b41a4008a32c523969d893b3da5
Nick <nick.benoit14@gmail.com>
2016-08-03 10:10:41 -0600

    add spec

--- client/js/components/common/file_upload.jsx
+++ client/js/components/common/file_upload.jsx
@@ -6,11 +6,14 @@ export default class FileUpload extends React.Component {
     selectAnswer: React.PropTypes.func,
 
     // User facing strings of the language specified by the 'locale' setting
-    localizedStrings: React.PropTypes.object.isRequired
-  }
+    // localizedStrings: React.PropTypes.object.isRequired TODO
+  };
 
   handleChange(e){
     if(_.isFunction(this.props.selectAnswer)){
+      // We are currently only allowing a single file upload. Without enabling
+      // multiple uploads on the file input, the file list will only be of size 1
+      // and will get replaced every time the file is updated.
       this.props.selectAnswer(e.target.files[0]);
     }
   }
