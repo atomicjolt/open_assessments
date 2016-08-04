90f30dc406371ffb178c52f19c5b259d1d56eaab
Nick <nick.benoit14@gmail.com>
2016-08-04 10:47:11 -0600

    add isNextUnlock ON_CORRECT specs

--- client/js/selectors/assessment.spec.js
+++ client/js/selectors/assessment.spec.js
@@ -1,4 +1,5 @@
 import Immutable                 from 'immutable';
+import _                         from 'lodash';
 import * as AssessmentSelectors  from "./assessment";
 
 
@@ -22,7 +23,7 @@ describe('isFirstPage', () => {
   });
 });
 
-fdescribe('isLastPage', () => {
+describe('isLastPage', () => {
   it('should return true on last question', () => {
     const currentItemIndex = 8;
     const numItems = 10;
@@ -41,5 +42,43 @@ fdescribe('isLastPage', () => {
   });
 });
 
-describe('isNextUnlocked', () => {});
+describe('isNextUnlocked', () => {
+  const createResponse = (i, responses) => {
+    responses[i] = {
+      answerIds:["1", "2"],
+      correct:true,
+      feedback:"<p>Feedbac</p>"
+    }
+  }
+  fdescribe('unlockNext is ON_CORRECT', () => {
+    var nextUnlocked, responses, questionsPerPage;
+    beforeEach(() => {
+      nextUnlocked = "ON_CORRECT";
+      responses = {};
+      questionsPerPage = 12;
+      _.range(12).forEach((i) =>{createResponse(i, responses)}); //TODO doc
+    })
+
+    it('should return true when all answers in current page are correct', () => {
+      const result = AssessmentSelectors._isNextUnlocked(nextUnlocked, responses, questionsPerPage);
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when there is an incorrect answer', () => {
+      responses[3].correct = false;
+      const result = AssessmentSelectors._isNextUnlocked(nextUnlocked, responses, questionsPerPage);
+      expect(result).toEqual(false);
+    });
+
+    it('should return false when there is an unanswered question', () => {
+      delete responses[4];
+      const result = AssessmentSelectors._isNextUnlocked(nextUnlocked, responses, questionsPerPage);
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('unlockNext is ON_ANSWER_CHECK', () => {});
+  describe('ALWAYS', () => {});
+});
+
 describe('currentItems', () => {});
