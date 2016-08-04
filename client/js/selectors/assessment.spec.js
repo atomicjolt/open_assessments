111c07919fc05099aa6aaecade5dc98b6d454bca
Nick <nick.benoit14@gmail.com>
2016-08-04 09:47:21 -0600

    add selector specs

---
+++ client/js/selectors/assessment.spec.js
@@ -0,0 +1,15 @@
+import * as AssessmentSelectors from "./assessment";
+
+
+describe('isFirstPage', () => {
+  it('should return true on first question', () => {});
+  it('should return false otherwise', () => {});
+});
+
+describe('isLastPage', () => {
+  it('should return true on last question', () => {});
+  it('should return false otherwise', () => {});
+});
+
+describe('isNextUnlocked', () => {});
+describe('currentItems', () => {});
