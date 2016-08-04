7d76debc3eb710d7fd4197d9d7b87bc3ad78348d
David Owen <dsowen@fugue88.ws>
2016-08-04 09:56:30 -0600

    Clean-ups

--- client/js/selectors/assessment.js
+++ client/js/selectors/assessment.js
@@ -1,15 +1,16 @@
 import { AssessmentFormats }  from "../parsers/assessment";
-import * as ClixSelectors  from "../parsers/clix/selectors";
-import * as EdxSelectors   from "../parsers/edX/selectors";
-import * as Qti1Selectors  from "../parsers/qti1/selectors";
-import * as Qti2Selectors  from "../parsers/qti2/selectors";
+import * as ClixSelectors     from "../parsers/clix/selectors";
+import * as EdxSelectors      from "../parsers/edX/selectors";
+import * as Qti1Selectors     from "../parsers/qti1/selectors";
+import * as Qti2Selectors     from "../parsers/qti2/selectors";
 
 
-const SELECTORS_MAP = {};
-SELECTORS_MAP[AssessmentFormats.CLIx] = ClixSelectors;
-SELECTORS_MAP[AssessmentFormats.EdX] = EdxSelectors;
-SELECTORS_MAP[AssessmentFormats.Qti1] = Qti1Selectors;
-SELECTORS_MAP[AssessmentFormats.Qti2] = Qti2Selectors;
+const SELECTORS_MAP = {
+  [AssessmentFormats.CLIx]: ClixSelectors,
+  [AssessmentFormats.EdX]:  EdxSelectors,
+  [AssessmentFormats.Qti1]: Qti1Selectors,
+  [AssessmentFormats.Qti2]: Qti2Selectors
+};
 
 function getSelectors (standard) {
   return SELECTORS_MAP[standard];
