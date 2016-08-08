1eb1e984af160293c8caa999bde5674902091869
Nick <nick.benoit14@gmail.com>
2016-08-08 13:27:37 -0600

    remove served videojs file

--- client/styles/styles.js
+++ client/styles/styles.js
@@ -1,4 +1,4 @@
 // This file exists to compile all css - less and scss - into one file
 require('./styles.less');
 require('./styles.scss');
-require('../assets/styles/style.css'); //TODO remove
+require('../assets/styles/style.css');
