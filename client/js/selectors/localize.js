38aa32057cd29be6af1c3c78716bcf2e8e11f22d
David Owen <dsowen@fugue88.ws>
2016-08-08 11:00:28 -0600

    Default to English if no locale set

--- client/js/selectors/localize.js
+++ client/js/selectors/localize.js
@@ -5,7 +5,7 @@ import locales          from '../locales/locales';
  * Returns localized strings based on 'locale' setting
  */
 export function localizeStrings(state, props){
-  var lang = state.settings.locale;
+  var lang = state.settings.locale || "en";
   var strings = new LocalizedStrings(locales());
   strings.setLanguage(lang);
   return strings;
