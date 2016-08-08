14f4263e523c463ac93a3b77ad8491bca9e8b832
Nick <nick.benoit14@gmail.com>
2016-08-08 12:41:22 -0600

    cleanup

--- client/styles/styles.js
+++ client/styles/styles.js
@@ -1,6 +1,4 @@
 // This file exists to compile all css - less and scss - into one file
 require('./styles.less');
 require('./styles.scss');
-require('../assets/Styles/style.css'); //TODO remove
-// require('../assets/Styles/video-js.css');
-// require('../assets/Styles/videojs-skin-colors.css');
+require('../assets/styles/style.css'); //TODO remove
