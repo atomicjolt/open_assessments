6f58fd51a4a659633b404a8aedbf0811af0c86e2
Nick <nick.benoit14@gmail.com>
2016-08-10 15:41:07 -0600

    update telugu translations

--- client/js/locales/te.js
+++ client/js/locales/te.js
@@ -2,46 +2,54 @@ export default {
   te: {
     assessment: {
       unansweredQuestionWarning: "హెచ్చరిక జవాబు లేని ప్రశ్నలు ఉన్నాయి", // Warning displayed on last question if there are unanswered questions
-      leavingQuizPopup: "Don’t leave!", // Text displayed on javascript window alert when a student navigates away from summative quiz
-      counterQuestion: "Question {0} of {1}", // Counter for when only a single question is displayed at a time
-      counterPage: "Page {0} of {1}", // Counter for when many questions are displayed at a time
+      leavingQuizPopup: "వదలొద్దు!", // Text displayed on javascript window alert when a student navigates away from summative quiz
+      counterQuestion: "ప్రశ్న {0} యొక్క {1}", // Counter for when only a single question is displayed at a time
+      counterPage: "పేజీ {0} యొక్క {1}", // Counter for when many questions are displayed at a time
+    },
+    remaining: {
+      one_left: "Answer 1 more question.",
+      many_left: "Answer {0} more questions.",
+      done: "Well done!"
     },
     assessmentComplete: {
-      complete: "Quiz Complete" // Text displayed when quiz is completed
+      complete: "క్విజ్ పూర్తి" // Text displayed when quiz is completed (Quiz Completed-క్విజ్ ముగిసింది)
     },
     twoButtonNav:{
-      previousButton: "మునుపటి", // Text displayed on previous questions button
-      nextButton: "తరువాత", // Text displayed on next questions button
+      previousButton: "మునుపటి", // Text displayed on previous questions button మునుపటి/పూర్వపు
+      nextButton: "తరువాత", // Text displayed on next questions button తరువాత/ తర్వాత
       checkAnswerButton: "సమాధానాన్ని తనిఖీ", // Text displayed on next questions button,
-      saveFileButton: "పత్రాన్ని దాచు", // Text displayed on save file button
-      saveAnswerButton: "Save Answer", // Text displayed on save answer button
-      submitButton: "Finish Quiz" // Text displayed on submit button
+      saveFileButton: "ఫైల్ సెవ్ు", // Text displayed on save file button ఫైల్ సెవ్ (ఫైల్ కాపాడుట)
+      saveAnswerButton: "జవాబు సేవ్", // Text displayed on save answer button జవాబు సేవ్(జవాబు కాపాడుట)
+      submitButton: "క్విజ్ ముగింపు" // Text displayed on submit button క్విజ్ ముగింపు(క్విజ్ సమాప్తం)
     },
     item:{
-      questionDirectionMany:"Choose ALL that apply.", // Question direction for multi answer question
-      questionDirectionAll:"Choose the BEST answer." // Question direction for single answer question
+      questionDirectionMany:"వర్తించే అన్ని ఎంచుకోండి", // Question direction for multi answer question
+      questionDirectionAll:"ఉత్తమ జవాబుని ఎంచుకోండి" // Question direction for single answer question
     },
     retriesExceeded: {
-      triesExceeded: "Too many tries" // Text displayed when a student has run out of quiz attempts
+      triesExceeded: "చాలా ప్రయత్నాలు" // Text displayed when a student has run out of quiz attempts
     },
     start: {
-      summativeInstruction: "Summative Quiz", // Instructions for summative quiz
-      formativeInstruction: "Formative Quiz", // Instructions for formative quiz
-      showWhatYouKnowInstruction: "Show What You Know", // Instructions for 'show what you know' style quiz
+      summativeInstruction: "సంక్షిప్తమైన క్విజ్", // Instructions for summative quiz
+      formativeInstruction: "నిర్మాణాత్మక క్విజ్", // Instructions for formative quiz
+      showWhatYouKnowInstruction: "నీకు (మీకు) ఏం తెలుసొ చూపించు", // Instructions for 'show what you know' style quiz
       startButton: "Start Quiz" // Text displayed on start quiz button
     },
     notFound: {
-      notFound: "Not Found" // Text displayed when a page is not found
+      notFound: "దొరకలేదు" // Text displayed when a page is not found
     },
     about: {
-      title:"Open Assessments" // About page title
+      title:"ఓపెన్ అసెస్మెంట్/ తెరిచిన లెక్కింపులు" // About page title
     },
     loading: {
-      loading: "Loading..."
+      loading: "లొడ్ అయ్తవుంది/ లోడ్ అవ్తుంది"
     },
     audioUpload: {
-      record: "Record",
-      stop: "Stop"
+      record: "రికార్డు",
+      stop: "ఆపు/ఆగు"
+    },
+    fileUpload: {
+      chooseFile: "Choose a File to Upload"
     }
   }
 };
