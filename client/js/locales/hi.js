8433cdb549712d1e0895a52bb7da3090b4163d6d
David Owen <dsowen@fugue88.ws>
2016-08-18 09:26:23 -0600

    Updated Hindi locale

--- client/js/locales/hi.js
+++ client/js/locales/hi.js
@@ -2,46 +2,54 @@ export default {
   hi: {
     assessment: {
       unansweredQuestionWarning: "चेतावनी! कुछ सवालों के जवाब नहीं हैं", // Warning displayed on last question if there are unanswered questions
-      leavingQuizPopup: "Don’t leave!", // Text displayed on javascript window alert when a student navigates away from summative quiz
-      counterQuestion: "Question {0} of {1}", // Counter for when only a single question is displayed at a time
-      counterPage: "Page {0} of {1}", // Counter for when many questions are displayed at a time
+      leavingQuizPopup: "ठहरिए!", // Text displayed on javascript window alert when a student navigates away from summative quiz
+      counterQuestion: "{1} की प्रश्न संख्या {0}", // Counter for when only a single question is displayed at a time
+      counterPage: "{1} की पृष्ठ संख्या {0}" // Counter for when many questions are displayed at a time
+    },
+    remaining: {
+      one_left: "1 और प्रश्न का उत्तर दें",
+      many_left: "{0} और प्रश्नों का उत्तर दें",
+      done: "शाबाश!"
     },
     assessmentComplete: {
-      complete: "Quiz Complete" // Text displayed when quiz is completed
+      complete: "प्रश्नोतरी समाप्त" // Text displayed when quiz is completed
     },
     twoButtonNav:{
       previousButton: "पिछला", // Text displayed on previous questions button
       nextButton: "अगला", // Text displayed on next questions button
       checkAnswerButton: "जवाब की जांच करो", // Text displayed on next questions button,
       saveFileButton: "फाइल सुरक्षित करें", // Text displayed on save file button
-      saveAnswerButton: "Save Answer", // Text displayed on save answer button
-      submitButton: "Finish Quiz" // Text displayed on submit button
+      saveAnswerButton: "उत्तर सेव करें", // Text displayed on save answer button
+      submitButton: "प्रश्नोतरी समाप्त करें" // Text displayed on submit button
     },
     item:{
-      questionDirectionMany:"Choose ALL that apply.", // Question direction for multi answer question
-      questionDirectionAll:"Choose the BEST answer." // Question direction for single answer question
+      questionDirectionMany:"लागू होने वाले प्रत्येक को चुनें", // Question direction for multi answer question
+      questionDirectionAll:"श्रेष्ठ उत्तर चुनें" // Question direction for single answer question
     },
     retriesExceeded: {
-      triesExceeded: "Too many tries" // Text displayed when a student has run out of quiz attempts
+      triesExceeded: "सीमा से अधिक प्रयत्न" // Text displayed when a student has run out of quiz attempts
     },
     start: {
-      summativeInstruction: "Summative Quiz", // Instructions for summative quiz
-      formativeInstruction: "Formative Quiz", // Instructions for formative quiz
-      showWhatYouKnowInstruction: "Show What You Know", // Instructions for 'show what you know' style quiz
-      startButton: "Start Quiz" // Text displayed on start quiz button
+      summativeInstruction: "निर्णायक प्रश्नोतरी", // Instructions for summative quiz
+      formativeInstruction: "सृजनात्मक प्रश्नोतरी", // Instructions for formative quiz
+      showWhatYouKnowInstruction: "दिखाइए आप क्या जानते हैं", // Instructions for 'show what you know' style quiz
+      startButton: "प्रश्नोत्तरी शुरू करें" // Text displayed on start quiz button
     },
     notFound: {
-      notFound: "Not Found" // Text displayed when a page is not found
+      notFound: "मिला नहीं" // Text displayed when a page is not found
     },
     about: {
-      title:"Open Assessments" // About page title
+      title:"खुला आकलन" // About page title
     },
     loading: {
-      loading: "Loading..."
+      loading: "लोड हो रहा है"
     },
     audioUpload: {
-      record: "Record",
-      stop: "Stop"
+      record: "दर्ज करें",
+      stop: "रुकिए"
+    },
+    fileUpload: {
+      chooseFile: "अपलोड करने के लिए एक फाइल चुनें"
     }
   }
 };
