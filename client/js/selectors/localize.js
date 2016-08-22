8f702040721a8700ddb5983a403159c65adda078
David Owen <dsowen@fugue88.ws>
2016-08-22 10:40:43 -0600

    Set the selector to look for a dynamically-set locale first, then the setting, then default to English

--- client/js/selectors/localize.js
+++ client/js/selectors/localize.js
@@ -4,7 +4,7 @@ import strings from "../locales/locales";
  * Returns localized strings based on 'locale' setting
  */
 export function localizeStrings(state, props) {
-  var lang = state.settings.locale || "en";
+  var lang = state.locale || state.settings.locale || "en";
   strings.setLanguage(lang);
   return strings;
 };
