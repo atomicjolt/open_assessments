3f9d361b3ef0cafb319d538cedf9e88186fada5c
bensmith <benhsmith13@gmail.com>
2017-02-10 07:30:30 -0700

    converts to class component

--- client/js/_author/components/assessments/question_types/_question.jsx
+++ client/js/_author/components/assessments/question_types/_question.jsx
@@ -5,27 +5,36 @@ import Settings         from './question_common/question_settings';
 import QuestionText     from './question_common/question_text';
 import Feedback         from './question_common/feedback';
 
-export default function (props) {
-  const content = () => {
-    if (!props.question) { return null; }
-    switch (props.question.type) {
-      case 'OsidObject':            //Kill this monster
-      case 'multipleChoice':
-        return <MultipleChoice {...props} />;
+export default class Question extends React.Component{
+  static propTypes = {
+    genusTypeId: React.PropTypes.string.isRequired,
+  };
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  content() {
+    switch (this.props.genusTypeId) {
+      case 'item-genus-type%3Aqti-choice-interaction%40ODL.MIT.EDU':
+        return <MultipleChoice {...this.props} />;
       default:
         return null;
     }
-  };
+  }
 
-  return (
-    <div className="o-item c-question is-active" tabIndex="0">
-      <InactiveHeader />
-      <Settings />
-      <div className="c-question__content">
-        <QuestionText />
-        {content()}
-        <Feedback />
+  render() {
+    return (
+      <div className="o-item c-question is-active" tabIndex="0">
+        <InactiveHeader />
+        <Settings />
+        <div className="c-question__content">
+          <QuestionText />
+          {this.content()}
+          <Feedback />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
