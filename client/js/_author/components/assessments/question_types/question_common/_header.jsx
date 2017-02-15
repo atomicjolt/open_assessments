fedf498b89a56cf5a3db25198f044cf7f7d05170
dspencer001 <dspencer577@gmail.com>
2017-02-15 14:41:48 -0700

    Move common code a level up

--- client/js/_author/components/assessments/question_types/question_common/_header.jsx
+++ client/js/_author/components/assessments/question_types/question_common/_header.jsx
@@ -12,14 +12,25 @@ export default function QuestionHeader(props) {
     }
   };
 
+  let currentHeader = <InactiveHeader {...props} type={type()} />;
+
   if (props.reorderActive) {
-    return <ReorderHeader {...props} type={type()} />;
+    currentHeader =  <ReorderHeader {...props} type={type()} />;
   }
 
-  return <InactiveHeader {...props} type={type()} />;
+  return (
+    <div className="o-item__top">
+      <div className="o-left">
+        <h3 className="c-question__number">Question {props.index + 1}</h3>
+        <div className="c-question__type">&nbsp;&nbsp; - &nbsp;&nbsp; {props.type}</div>
+      </div>
+      { currentHeader }
+    </div>
+  );
 }
 
 QuestionHeader.propTypes = {
   reorderActive: React.PropTypes.bool,
   type: React.PropTypes.string,
+  index: React.PropTypes.number,
 };
--- client/js/_author/components/assessments/question_types/question_common/reorder_h.jsx
+++ client/js/_author/components/assessments/question_types/question_common/reorder_h.jsx
@@ -5,37 +5,28 @@ export default function inactiveHeader(props) {
   const hideDown = props.bottomItem ? 'is-hidden' : '';
 
   return (
-    <div className="o-item__top">
-      <div className="o-left">
-        <h3 className="c-question__number">Question {props.index + 1}</h3>
-        <div className="c-question__type">&nbsp;&nbsp; - &nbsp;&nbsp; {props.type}</div>
-      </div>
-
-      <div className="o-right c-question-icons c-question-icons--reorder">
-        <button
-          className={`c-btn c-btn--square ${hideUp}`}
-          onClick={!props.topItem && props.moveUp}
-        >
-          <i className="material-icons">arrow_upward</i>
-        </button>
-        <button
-          className={`c-btn c-btn--square ${hideDown}`}
-          onClick={!props.bottomItem && props.moveDown}
-        >
-          <i className="material-icons">arrow_downward</i>
-        </button>
-        <button
-          onClick={props.toggleReorder}
-          className="c-btn c-btn--sm c-btn--white"
-        > Done </button>
-      </div>
+    <div className="o-right c-question-icons c-question-icons--reorder">
+      <button
+        className={`c-btn c-btn--square ${hideUp}`}
+        onClick={!props.topItem && props.moveUp}
+      >
+        <i className="material-icons">arrow_upward</i>
+      </button>
+      <button
+        className={`c-btn c-btn--square ${hideDown}`}
+        onClick={!props.bottomItem && props.moveDown}
+      >
+        <i className="material-icons">arrow_downward</i>
+      </button>
+      <button
+        onClick={props.toggleReorder}
+        className="c-btn c-btn--sm c-btn--white"
+      > Done </button>
     </div>
   );
 }
 
 inactiveHeader.propTypes = {
-  name: React.PropTypes.string,
-  type: React.PropTypes.string,
   topItem: React.PropTypes.bool,
   bottomItem: React.PropTypes.bool,
   toggleReorder: React.PropTypes.func,
