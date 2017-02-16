7643c9fda0e68eada1e0a4b35f0efee4938a87d1
dspencer001 <dspencer577@gmail.com>
2017-02-15 17:35:19 -0700

    Remove fdescribe

--- client/js/_author/components/assessments/question_types/question_common/header/reorder.spec.jsx
+++ client/js/_author/components/assessments/question_types/question_common/header/reorder.spec.jsx
@@ -4,7 +4,7 @@ import _             from 'lodash';
 import Stub         from '../../../../../../../specs_support/stub';
 import ReorderHeader from './reorder';
 
-fdescribe('', () => {
+describe('', () => {
   let result;
   let props;
   let called;
