acdfefb0f17d3bba6a9a1004f53cda9e13a0ccc3
Nick Benoit <nick.benoit14@gmail.com>
2017-02-20 08:26:39 -0700

    adds assessment preview component

---
+++ client/js/_author/components/assessments/assessment_preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import Assessment from '../../../_player/componentsi/main/assessment.jsx';
+
+export default class ItemPreview extends React.Component {
+  static propTypes = {
+  }
+
+  render() {
+    return <div> Howdy </div>;
+  }
+}
--- client/js/_author/components/assessments/navigation_bar_content.jsx
+++ client/js/_author/components/assessments/navigation_bar_content.jsx
@@ -2,49 +2,65 @@ import React            from 'react';
 import _                from 'lodash';
 import { hashHistory }  from 'react-router';
 import Icon             from '../bank_navigation/bank_icon';
+import Preview          from './assessment_preview';
 
-export default function AssessmentsView(props) {
+export default class AssessmentsView extends React.Component {
+  static propTypes = {
+    editOrPublishAssessment: React.PropTypes.func.isRequired,
+    isPublished: React.PropTypes.bool.isRequired,
+    items: React.PropTypes.shape({}).isRequired,
+  };
 
-  const publishButton = () => {
-    if (!_.isEmpty(props.items)) {
+  static getPreview(stuff) {
+    if (stuff) return <Preview />;
+    return null;
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      shouldDisplayPreview: false,
+    };
+  }
+
+  publishButton() {
+    if (!_.isEmpty(this.props.items)) {
       return (
         <button
           className="c-btn c-btn--sm c-btn--green"
-          onClick={() => props.editOrPublishAssessment(props.isPublished)}
+          onClick={() => this.props.editOrPublishAssessment(this.props.isPublished)}
         >
-          <Icon type={props.isPublished ? 'Published' : 'Publish'} />
-          {props.isPublished ? 'Unpublish' : 'Publish'}
+          <Icon type={this.props.isPublished ? 'Published' : 'Publish'} />
+          {this.props.isPublished ? 'Unpublish' : 'Publish'}
         </button>
       );
     }
     return null;
-  };
+  }
 
-  return (
-    <div className="c-header-bottom">
-      <div className="c-header-bottom__left">
-        <button
-          onClick={() => hashHistory.goBack()}
-          className="c-btn c-btn--sm c-btn--outline c-btn--back"
-        >
-          <i className="material-icons">keyboard_arrow_left</i>
-          Back
-        </button>
-      </div>
 
-      <div className="c-header-bottom__right">
-        { publishButton() }
-        <button className="c-btn c-btn--sm c-btn--maroon u-ml-md">
-          <i className="material-icons">remove_red_eye</i>
-          Preview Assessment
-        </button>
+  render() {
+    return (
+      <div className="c-header-bottom">
+        <div className="c-header-bottom__left">
+          <button
+            onClick={() => hashHistory.goBack()}
+            className="c-btn c-btn--sm c-btn--outline c-btn--back"
+          >
+            <i className="material-icons">keyboard_arrow_left</i>
+            Back
+          </button>
+        </div>
+
+        <div className="c-header-bottom__right">
+          { this.publishButton() }
+          <button className="c-btn c-btn--sm c-btn--maroon u-ml-md">
+            <i className="material-icons">remove_red_eye</i>
+            Preview Assessment
+          </button>
+        </div>
+        {AssessmentsView.getPreview(this.state.shouldDisplayPreview)}
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-AssessmentsView.propTypes = {
-  editOrPublishAssessment: React.PropTypes.func.isRequired,
-  isPublished: React.PropTypes.bool.isRequired,
-  items: React.PropTypes.shape({}).isRequired,
-};
