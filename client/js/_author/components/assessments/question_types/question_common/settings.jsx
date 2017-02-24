c1d7e23041c36c06e8bd970e382de6db2a837617
BenjaminCall <llacneb@gmail.com>
2017-02-24 13:13:58 -0800

    checkboxes for only multiple choice for now (#155)

--- client/js/_author/components/assessments/question_types/question_common/settings.jsx
+++ client/js/_author/components/assessments/question_types/question_common/settings.jsx
@@ -1,40 +1,10 @@
 import React    from 'react';
+import genusTypes from '../../../../../constants/genus_types';
 
 export default function questionSettings(props) {
-  return (
-    <div className="c-question-settings">
-      <div className="o-left">
-        <div className="c-input c-input-label--left">
-          <label htmlFor={`question_name_${props.id}`}>Name</label>
-          <div className="c-input__contain">
-            <input
-              className="c-text-input c-text-input--smaller"
-              id={`question_name_${props.id}`}
-              type="text"
-              tabIndex="0"
-              defaultValue={props.defaultName}
-              onBlur={e => props.updateItem({ name: e.target.value })}
-            />
-            <div className="c-input__bottom" />
-          </div>
-        </div>
 
-        <div className="c-dropdown c-dropdown--small u-ml-md">
-          <select
-            name=""
-            id=""
-            tabIndex="0"
-            value={props.language}
-            onChange={e => props.updateState('nameLanguage', e.target.value)}
-            onBlur={props.updateItem}
-          >
-            <option value="639-2%3AENG%40ISO">English</option>
-            <option value="">French</option>
-            <option value="">Spanish</option>
-          </select>
-        </div>
-      </div>
-
+  function checkboxOptions() {
+    return (props.genusTypeId === genusTypes.item.multipleChoice ?
       <div className="o-right">
         <div className="c-checkbox u-ml-md">
           <input
@@ -69,7 +39,44 @@ export default function questionSettings(props) {
           />
           <label htmlFor={`check04_${props.id}`}>Reflection</label>
         </div>
+      </div> : null
+    );
+  }
+
+  return (
+    <div className="c-question-settings">
+      <div className="o-left">
+        <div className="c-input c-input-label--left">
+          <label htmlFor={`question_name_${props.id}`}>Name</label>
+          <div className="c-input__contain">
+            <input
+              className="c-text-input c-text-input--smaller"
+              id={`question_name_${props.id}`}
+              type="text"
+              tabIndex="0"
+              defaultValue={props.defaultName}
+              onBlur={e => props.updateItem({ name: e.target.value })}
+            />
+            <div className="c-input__bottom" />
+          </div>
+        </div>
+
+        <div className="c-dropdown c-dropdown--small u-ml-md">
+          <select
+            name=""
+            id=""
+            tabIndex="0"
+            value={props.language}
+            // onChange={e => props.updateState('nameLanguage', e.target.value)}
+            onBlur={props.updateItem}
+          >
+            <option value="639-2%3AENG%40ISO">English</option>
+            <option value="">Hindi</option>
+            <option value="">Telugu</option>
+          </select>
+        </div>
       </div>
+      {checkboxOptions()}
     </div>
   );
 }
@@ -77,8 +84,10 @@ export default function questionSettings(props) {
 questionSettings.propTypes = {
   id: React.PropTypes.string.isRequired,
   defaultName: React.PropTypes.string,
+  genusTypeId: React.PropTypes.string,
   language: React.PropTypes.string.isRequired,
   updateItem: React.PropTypes.func.isRequired,
+  // updateState: React.PropTypes.func.isRequired,
   maintainOrder: React.PropTypes.bool,
   multipleAnswer: React.PropTypes.bool,
   reflection: React.PropTypes.bool,
