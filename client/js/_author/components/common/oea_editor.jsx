ba07958dd6c1600da212f3f0c42a2d32db18a08e
dspencer001 <dspencer001@users.noreply.github.com>
2017-02-28 16:54:12 -0700

    Add editor (#143)
    * Add the wysiwyg editor with default styles
    
    * Add uploading images using the wysiwyg editor
    
    * Extract out the editor to a wrapped connected editor
    
    * Don't pass unnecessary upload image
    
    * Delete unnecessary styles entry point
    
    * Remove unnecessary changes
    
    * Move to using tinymce
    
    * Push up in progress stuff
    
    * Fixed random p tag insertion
    
    * Go back to inputs for options for now
    
    * Add non upload support for drag and drop images
    
    * Don't import yet
    
    * Update comments
    
    * Remove unnecessary draft packages
    
    * Update comment again
    
    * Fix spec...
    
    * Fix all the things
    
    * Make things work with the deserializer

---
+++ client/js/_author/components/common/oea_editor.jsx
@@ -0,0 +1,43 @@
+import React       from 'react';
+import TinyWrapper from './tiny_wrapper';
+
+export default class OeaEditor extends React.Component {
+  static propTypes = {
+    onBlur: React.PropTypes.func.isRequired,
+  };
+
+  constructor() {
+    super();
+
+    this.state = {
+      focused: false
+    };
+  }
+
+  onBlur(editorText) {
+    // TODO: This is where we will handle replacing the image source with the
+    // "AssetContent:{asset_id}". We need to do it here instead of in the
+    // tinymce callback because we want to be able to preview the uploads.
+
+    this.setState({ focused: false });
+    this.props.onBlur(editorText);
+  }
+
+  render() {
+    // active is handled here instead of in TinyWrapper, because tinyMCE does
+    // not handle rerendering very well. This may become unnecessary when
+    // Brandon finishes the styles.
+
+    const active = this.state.focused ? 'is-focused' : '';
+
+    return (
+      <div className={`c-text-input c-text-input--medium c-wysiwyg ${active}`}>
+        <TinyWrapper
+          {...this.props}
+          onBlur={editorText => this.onBlur(editorText)}
+          onFocus={() => this.setState({ focused: true })}
+        />
+      </div>
+    );
+  }
+}
