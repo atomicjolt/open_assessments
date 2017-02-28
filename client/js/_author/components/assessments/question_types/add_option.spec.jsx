867e1003c999f7b0e574dbe30baeaf3b51d4b3fc
Austin Adams <austin.adams@aggiemail.usu.edu>
2017-02-28 16:36:59 -0700

    deleted extra comments

--- client/js/_author/components/assessments/question_types/add_option.spec.jsx
+++ client/js/_author/components/assessments/question_types/add_option.spec.jsx
@@ -21,38 +21,4 @@ fdescribe('Update Choice working', () => {
     TestUtils.Simulate.click(div);
     expect(called).toBeTruthy();
   });
-  //
-  // it('sets default settings on new assessments', () => {
-  //   const button = TestUtils.findRenderedDOMComponentWithClass(result, 'c-btn--maroon');
-  //   TestUtils.Simulate.click(button);
-  //   expect(called.genusTypeId).toBeDefined();
-  //   expect(called.genusTypeId).toBe('item-genus-type%3Aqti-choice-interaction%40ODL.MIT.EDU');
-  //   expect(called.name).toBe('');
-  //   expect(called.language).toBe('english');
-  // });
-  //
-  // it('sets name on new assessments', () => {
-  //   const input = TestUtils.findRenderedDOMComponentWithClass(result, 'c-text-input');
-  //   const button = TestUtils.findRenderedDOMComponentWithClass(result, 'c-btn--maroon');
-  //   TestUtils.Simulate.change(input, { target: { value: 'tacos' } });
-  //   TestUtils.Simulate.click(button);
-  //   expect(called.name).toBe('tacos');
-  // });
-  //
-  // it('sets type on new assessments', () => {
-  //   const input = TestUtils.scryRenderedDOMComponentsWithTag(result, 'select')[0];
-  //   const button = TestUtils.findRenderedDOMComponentWithClass(result, 'c-btn--maroon');
-  //   TestUtils.Simulate.change(input, { target: { value: 'item-genus-type%3Aqti-choice-interaction%40ODL.MIT.EDU' } });
-  //   TestUtils.Simulate.click(button);
-  //   expect(called.genusTypeId).toBe('item-genus-type%3Aqti-choice-interaction%40ODL.MIT.EDU');
-  // });
-  //
-  // it('sets language on new assessments', () => {
-  //   const input = TestUtils.scryRenderedDOMComponentsWithTag(result, 'select')[1];
-  //   const button = TestUtils.findRenderedDOMComponentWithClass(result, 'c-btn--maroon');
-  //   TestUtils.Simulate.change(input, { target: { value: 'french' } });
-  //   TestUtils.Simulate.click(button);
-  //   expect(called.language).toBe('french');
-  // });
-
 });
