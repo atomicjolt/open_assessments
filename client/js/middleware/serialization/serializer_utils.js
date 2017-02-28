5f2a8df888eb14970fcdf7d82b803021a0d75214
bensmith <benhsmith13@gmail.com>
2017-02-28 13:36:25 -0700

    Checks before removing

--- client/js/middleware/serialization/serializer_utils.js
+++ client/js/middleware/serialization/serializer_utils.js
@@ -3,6 +3,6 @@ import _    from 'lodash';
 export function scrub(item) {
   let scrubbedItem = _.cloneDeep(item);
   scrubbedItem = _.omitBy(scrubbedItem, _.isNil);
-  scrubbedItem = _.omitBy(scrubbedItem, _.isEmpty);
+  scrubbedItem = _.omitBy(scrubbedItem, temp => _.isObject(temp) && _.isEmpty(temp));
   return scrubbedItem;
 }
