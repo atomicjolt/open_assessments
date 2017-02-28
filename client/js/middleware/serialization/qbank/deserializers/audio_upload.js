5363ae8c17e658c308523a648fded79cfa5dd7d4
Nick Benoit <nick.benoit14@gmail.com>
2017-02-28 16:30:19 -0700

    update timeValue

--- client/js/middleware/serialization/qbank/deserializers/audio_upload.js
+++ client/js/middleware/serialization/qbank/deserializers/audio_upload.js
@@ -4,13 +4,15 @@ import genusTypes         from '../../../../constants/genus_types';
 
 export default function audioUpload(item) {
   const newItem = baseDeserializer(item);
-  _.merge(newItem.question, { // TODO update value
+  const timeValue = _.get(item, 'question.timeValue', {
     timeValue: {
       hours: '00',
       minutes: '00',
       seconds: '100'
     }
   });
+
+  _.merge(newItem.question, { timeValue });
 
   return newItem;
 }
