aa3f4d07ea8da4cc7170759ef08431dd1ea55539
Nick Benoit <nick.benoit14@gmail.com>
2017-03-01 12:24:33 -0700

    improves baseItem serializer

--- client/js/middleware/serialization/qbank/serializers/base.js
+++ client/js/middleware/serialization/qbank/serializers/base.js
@@ -1,14 +1,5 @@
 import genusTypes       from '../../../../constants/genus_types';
-
-export function baseSerializeItem(originalItem, newAttributes) {
-  return {
-    id: originalItem.id,
-    genusTypeId: genusTypes.item[originalItem.type],
-    name: newAttributes.name,
-    question: null,
-    answers: null,
-  };
-}
+import { scrub }        from '../../serializer_utils';
 
 export function baseSerializeQuestion(originalItem, newAttributes) {
   return {
@@ -16,6 +7,22 @@ export function baseSerializeQuestion(originalItem, newAttributes) {
     genusTypeId: genusTypes.question[originalItem.type],
     questionString: newAttributes.text,
   };
+}
+
+export function baseSerializeItem(originalItem, newAttributes) {
+  const newItem = {
+    id: originalItem.id,
+    genusTypeId: genusTypes.item[originalItem.type],
+    name: newAttributes.name || originalItem.name,
+    question: baseSerializeQuestion(originalItem, newAttributes.question || {}),
+    answers: null,
+  };
+  newItem.question = scrub(newItem.question);
+  return newItem;
+}
+
+export function baseItem(originalItem, newAttributes) {
+  return scrub(baseSerializeItem(originalItem, newAttributes));
 }
 
 export default baseSerializeItem;
