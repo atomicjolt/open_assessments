cbea9aa032e5c3142db29fe69fa5f1eef49ab1d0
Ben Smith <ben.smith@atomicjolt.com>
2017-03-01 21:45:54 -0700

    Multi selection (#179)
    * This should handle all the different types
    
    * Swapping types works
    
    * Swapping types works
    
    * this
    
    * better serialization for a better tomorrow

--- client/js/_author/components/assessments/question_types/_question.jsx
+++ client/js/_author/components/assessments/question_types/_question.jsx
@@ -3,10 +3,10 @@ import MultipleChoice   from './multiple_choice';
 import QuestionHeader   from './question_common/header/_header';
 import Settings         from './question_common/settings';
 import QuestionText     from './question_common/text';
-import genusTypes       from '../../../../constants/genus_types.js';
 import AudioUpload      from './audio_upload';
 import FileUpload       from './file_upload';
 import ShortAnswer      from './short_answer';
+import types            from '../../../../constants/question_types';
 
 export default class Question extends React.Component {
   static propTypes = {
@@ -55,15 +55,10 @@ export default class Question extends React.Component {
     this.props.updateItem({ id: item.id, ...newItemProperties });
   }
 
-  makeReflection(reflect) {
-    const { item } = this.props;
-    let type = 'multipleChoice';
-    if (reflect) {
-      type = item.multipleAnswer ? 'multipleReflection' : 'reflection';
-    }
+  changeType(type) {
     // The choices: true is to make sure the deserializer updates the choice and answer data
     this.props.updateItem({
-      id: item.id,
+      id: this.props.item.id,
       type,
       question: {
         type,
@@ -72,10 +67,42 @@ export default class Question extends React.Component {
     });
   }
 
+  makeReflection() {
+    const { item } = this.props;
+    let type = 'multipleChoice';
+    if (item.type === types.multipleChoice) {
+      type = types.reflection;
+    } else if (item.type === types.reflection) {
+      type = types.multipleChoice;
+    } else if (item.type === types.multipleReflection) {
+      type = types.multipleAnswer;
+    } else if (item.type === types.multipleAnswer) {
+      type = types.multipleReflection;
+    }
+    this.changeType(type);
+  }
+
+  makeMultipleAnswer() {
+    const { item } = this.props;
+    let type = 'multipleChoice';
+    if (item.type === types.multipleChoice) {
+      type = types.multipleAnswer;
+    } else if (item.type === types.reflection) {
+      type = types.multipleReflection;
+    } else if (item.type === types.multipleReflection) {
+      type = types.reflection;
+    } else if (item.type === types.multipleAnswer) {
+      type = types.multipleChoice;
+    }
+    this.changeType(type);
+  }
+
   content() {
     switch (this.props.item.type) {
-      case 'multipleChoice':
-      case 'reflection':
+      case types.multipleChoice:
+      case types.reflection:
+      case types.multipleReflection:
+      case types.multipleAnswer:
         return (
           <MultipleChoice
             {...this.props}
@@ -84,14 +111,14 @@ export default class Question extends React.Component {
             isActive={this.props.isActive}
           />
         );
-      case 'audioUpload':
+      case types.audioUpload:
         return (
           <AudioUpload
             updateItem={newProps => this.updateItem(newProps)}
             item={this.props.item}
           />
         );
-      case 'fileUpload':
+      case types.fileUpload:
         return (
           <FileUpload
             updateItem={newProps => this.updateItem(newProps)}
@@ -99,7 +126,7 @@ export default class Question extends React.Component {
           />
         );
 
-      case 'shortAnswer':
+      case types.shortAnswer:
         return (
           <ShortAnswer
             updateItem={newProps => this.updateItem(newProps)}
@@ -145,9 +172,10 @@ export default class Question extends React.Component {
           defaultName={name}
           language={language}
           maintainOrder={question && !question.shuffle}
-          multipleAnswer={item.multipleAnswer}
-          reflection={item.type === 'reflection'}
+          multipleAnswer={item.type === types.multipleAnswer || item.type === types.multipleReflection}
+          reflection={_.includes([types.reflection, types.multipleReflection], item.type)}
           makeReflection={reflect => this.makeReflection(reflect)}
+          makeMultipleAnswer={multi => this.makeMultipleAnswer(multi)}
           type={type}
         />
         <div className={`c-question__content ${this.props.reorderActive ? 'is-reordering' : ''}`}>
--- client/js/middleware/serialization/qbank/serializers/factory.js
+++ client/js/middleware/serialization/qbank/serializers/factory.js
@@ -3,21 +3,26 @@ import { baseItem }     from './base';
 import shortAnswer      from './short_answer';
 import audioUpload      from './audio_upload';
 import survey           from './survey';
+import multipleAnswer   from './multiple_answer';
+import types            from '../../../../constants/question_types.js';
 
 export default function factory(type) {
   switch (type) {
-    case 'multipleChoice':
+    case types.multipleChoice:
       return multipleChoice;
 
-    case 'shortAnswer':
+    case types.shortAnswer:
       return shortAnswer;
 
-    case 'audioUpload':
+    case types.audioUpload:
       return audioUpload;
 
-    case 'reflection':
-    case 'multipleReflection':
+    case types.reflection:
+    case types.multipleReflection:
       return survey;
+
+    case types.multipleAnswer:
+      return multipleAnswer;
 
     default:
       return baseItem;
