7f8c6617290848b0b2f1580cd06e80aad31014b8
Nick Benoit <nick.benoit14@gmail.com>
2017-03-01 20:50:39 -0700

    cleanup preview container

--- client/js/_author/components/assessments/preview_container.jsx
+++ client/js/_author/components/assessments/preview_container.jsx
@@ -6,75 +6,41 @@ import localizeStrings from '../../../_player/selectors/localize';
 import * as selectors from '../../../_player/selectors/assessment';
 import Parser from '../../../parsers/clix/parser';
 
-// function parsePreviewItem(item){
-//   return item;
-// }
-//
-// function parsePreview(items) {
-//   return items.map(parsePreviewItem);
-// }
-
 export default class PreviewContainer extends React.Component {
   static propTypes = {
-    // assessment: React.PropTypes.object,
-    // getAssessmentOffered: React.PropTypes.func,
-    // settings: React.PropTypes.object
+    previewItems: React.PropTypes.array.isRequired,
   }
 
   render() {
-    // if(this.props.previewItems.length > 0){
-      // const questions = selectors.questions({
-      //   assessment: {
-      //     standard: "CLIx",
-      //     items: this.props.previewItems,
-      //   },
-      // });
-      const assessment = Parser.parse("fakeid", {data: this.props.previewItems});
-      // debugger;
-      const questions = selectors.questions({assessment});
-      // debugger;
+    const assessment = Parser.parse('preview', { data: this.props.previewItems });
+    const questions = selectors.questions({ assessment });
 
-    // parsePreview(this.props.previewItems);
-
-    // const question = {
-    //   title:"Test Question Title",
-    //   material:"Test Question Material"
-    // };
-    const questionResult = {};
-    const currentItemIndex = 0;
-    // const assessment = {};
-    const questionCount = 10;
-    const index = 1
-    const settings = {};
-
-    const result = questions.map((question) => {
-      return <Item
-          localizedStrings={localizeStrings({settings:{locale:"en"}})}
-          key              = {index /* react uses this to distinguish children */}
-          settings         = {settings}
-          assessment       = {{}}
-          question         = {question}
-          response         = {[]}
-          currentItemIndex = {index}
-          questionCount    = {10}
-          questionResult   = {{}}
-          allQuestions     = {[question]}
-          outcomes         = {{}}
-          sendSize         = {() => {}}
-          videoPlay        = {() => {}}
-          videoPause       = {() => {}}
-          audioPlay        = {() => {}}
-          audioPause       = {() => {}}
-          audioRecordStart = {() => {}}
-          audioRecordStop  = {() => {}}
-          selectAnswer     = {
-            (answerId, exclusive) =>
-              {this.props.answerSelected(index, answerId, exclusive);}}
+    const result = questions.map((question, index) =>
+      <Item
+        localizedStrings={localizeStrings({ settings:{ locale:"en" } })}
+        key={ `item_${index}` }
+        settings={{}}
+        assessment={{}}
+        question={question}
+        response={[]}
+        currentItemIndex={index}
+        questionCount={0}
+        questionResult={{}}
+        allQuestions={[question]}
+        outcomes={{}}
+        sendSize={() => {}}
+        videoPlay={() => {}}
+        videoPause={() => {}}
+        audioPlay={() => {}}
+        audioPause={() => {}}
+        audioRecordStart={() => {}}
+        audioRecordStop={() => {}}
+        selectAnswer={() => {}}
       />
-    });
+    );
 
-    return <div>
+    return (<div>
       {result}
-    </div>;
+    </div>);
   }
 }
