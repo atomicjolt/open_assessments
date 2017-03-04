2620667eb595d6d98991fb820de3ddc239f2d7ea
Nick Benoit <nick.benoit14@gmail.com>
2017-03-04 12:33:37 -0700

    fix audio question serialization

--- client/js/middleware/serialization/qbank/serializers/audio_upload.js
+++ client/js/middleware/serialization/qbank/serializers/audio_upload.js
@@ -1,35 +1,23 @@
-import _                                             from 'lodash';
-import { baseSerializeItem, baseSerializeQuestion }  from './base';
-import { scrub, getSingleCorrectAnswer }            from '../../serializer_utils';
-
-const defaultTimeValue = {
-  hours: 0,
-  minutes: 0,
-  seconds: 240
-};
-
-function serializeQuestion(originalItem, newQuestionAttributes) {
-  const newQuestion = baseSerializeQuestion(originalItem, newQuestionAttributes);
-  if (newQuestionAttributes.timeValue) {
-    newQuestion.timeValue = newQuestionAttributes.timeValue;
-  }
-
-  return scrub(newQuestion);
-}
+import _                                        from 'lodash';
+import { baseItem }                             from './base';
+import { scrub, getSingleCorrectAnswer }        from '../../serializer_utils';
 
 export default function audioUploadSerializer(originalItem, newItemAttributes) {
-  const newItem = baseSerializeItem(originalItem, newItemAttributes);
-
   const { question } = newItemAttributes;
 
+  const newItem = baseItem(originalItem, newItemAttributes);
 
   if (question) {
     if (question.correctFeedback) {
       newItem.answers = [getSingleCorrectAnswer(originalItem, question)];
     }
 
-    newItem.question = serializeQuestion(originalItem, question);
+    if (!_.isEmpty(_.get(newItemAttributes, 'question.timeValue', {}))) {
+      newItem.question.timeValue = _.merge(
+        {},
+        newItemAttributes.question.timeValue
+      );
+    }
   }
-
   return scrub(newItem);
 }
