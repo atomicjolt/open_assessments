d47b4cf623e4c4ef0c67530c2287306772288f1d
dspencer001 <dspencer577@gmail.com>
2017-03-09 11:52:07 -0700

    Add a spec to test for the loader

--- client/js/_author/components/common/editor_upload_modal.spec.jsx
+++ client/js/_author/components/common/editor_upload_modal.spec.jsx
@@ -15,6 +15,7 @@ describe('editor upload modal', () => {
       mediaName: 'filename.jpg',
       insertMedia: () => { functionCalled = true; },
       uploadMedia: (file) => { fileUploaded = file },
+      inProgress: false,
     }
     result = shallow(<Modal {...props} />);
   });
@@ -58,5 +59,11 @@ describe('editor upload modal', () => {
   it('calls uploadMedia when a file is uploaded', () => {
     result.find('input').simulate('change', { target: { files: ["file"] } });
     expect(fileUploaded).toBe('file');
+  });
+
+  it('displays the loader when inProgress is true', () => {
+    props.inProgress = true;
+    result = shallow(<Modal {...props} />);
+    expect(result.find('dotLoader').length).toBe(1);
   });
 });
