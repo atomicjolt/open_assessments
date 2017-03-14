89e9cfcf69fdfe804f08fa981fccb9a2572ec629
Nick Benoit <nick.benoit14@gmail.com>
2017-03-13 18:21:56 -0600

    add timeValue to serializer

--- client/js/middleware/serialization/qbank/deserializers/moveable_words_sandbox.js
+++ client/js/middleware/serialization/qbank/deserializers/moveable_words_sandbox.js
@@ -1,8 +1,24 @@
 // Should be mostly the same and MW sentance, except needs to display audio time limit
 
+import _                  from 'lodash';
+
 import baseDeserializer   from './base';
+import { audioLimit }     from '../../../../constants/question_types';
+
 
 export default function fileUpload(item) {
   const newItem = baseDeserializer(item);
+  const timeValue = _.get(item, 'question.timeValue', {
+    hours: '00',
+    minutes: '00',
+    seconds: _.toString(audioLimit)
+  });
+
+  newItem.question = {
+    ...newItem.question,
+    timeValue
+  };
+
+
   return newItem;
 }
--- client/js/middleware/serialization/qbank/serializers/moveable_words_sandbox.js
+++ client/js/middleware/serialization/qbank/serializers/moveable_words_sandbox.js
@@ -1,7 +1,15 @@
-// Needs to send up timeValue
-
+import _ from 'lodash';
 import { baseItem } from './base';
 
 export default function movableWordsSerializer(originalItem, newItemAttributes) {
-  return baseItem(originalItem, newItemAttributes);
+  const newItem = baseItem(originalItem, newItemAttributes);
+
+  if (!_.isEmpty(_.get(newItemAttributes, 'question.timeValue', {}))) {
+    newItem.question.timeValue = _.merge(
+      {},
+      newItemAttributes.question.timeValue
+    );
+  }
+
+  return newItem;
 }
