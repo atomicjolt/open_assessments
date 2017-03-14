ef6c518132a46ac95976cb2169f0c94cb343c27c
Nick Benoit <nick.benoit14@gmail.com>
2017-03-14 11:18:57 -0600

    adds addOption

--- client/js/_author/components/assessments/question_types/moveable_words_sandbox/moveable_words_sandbox.jsx
+++ client/js/_author/components/assessments/question_types/moveable_words_sandbox/moveable_words_sandbox.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import _ from 'lodash';
+
 import AudioLimit from '../question_common/audio_limit';
 import Option from './option';
+import AddOption from './add_option';
 
 export default class MWSandbox extends React.Component {
   static propTypes = {
     item: React.PropTypes.object.isRequired,
     updateItem: React.PropTypes.func.isRequired,
+    updateChoice: React.PropTypes.func.isRequired,
     selectChoice: React.PropTypes.func.isRequired,
     blurOptions: React.PropTypes.func.isRequired,
     activeChoice: React.PropTypes.bool,
@@ -23,6 +27,31 @@ export default class MWSandbox extends React.Component {
     });
   }
 
+  // getChoices(choices) {
+  //   const { question } = this.props.item;
+  //   return _.each(choices, (choice) => {
+  //     return (
+  //       <Option
+  //         key={`assessmentChoice_${choice.id}`}
+  //         {...choice}
+  //         updateChoice={() => {}/* (newChoice, fileIds) => this.props.updateChoice(id, choice.id, newChoice, fileIds)*/}
+  //         updateItem={() => this.props.updateItem({ question })}
+  //         deleteChoice={() => this.deleteChoice(choice)}
+  //         shuffle={false /* question.shuffle*/}
+  //         moveUp={()=>{} /* () => this.moveChoice(choice, true) */}
+  //         moveDown={()=>{} /* () => this.moveChoice(choice)*/}
+  //         first={choice.order === 0}
+  //         last={question ? choice.order === _.size(question.choices) - 1 : true}
+  //         bankId={this.props.item.bankId}
+  //         itemId={this.props.item.id}
+  //         questionFileIds={question.fileIds}
+  //         setActiveChoice={choiceId => this.props.selectChoice(choiceId)}
+  //         isActive={this.props.isActive && choice.id === this.props.activeChoice}
+  //       />
+  //     );
+  //   });
+  // }
+
   render() {
     return (
       <div>
@@ -35,20 +64,7 @@ export default class MWSandbox extends React.Component {
         <div className="au-c-question__answers au-c-moveable__answers">
           <Option isActive />
           <Option />
-          <div className="au-c-answer au-o-flex-center au-c-answer--add">
-            <div className="au-c-input">
-              <label htmlFor="option2" />
-              <div className="au-c-input__contain">
-                <input
-                  className="au-c-text-input au-c-text-input--small au-c-wysiwyg au-c-option"
-                  id="option2"
-                  type="text"
-                  defaultValue="Add Option"
-                />
-                <div className="au-c-input__bottom no-border" />
-              </div>
-            </div>
-          </div>
+          <AddOption />
         </div>
       </div>
     );
