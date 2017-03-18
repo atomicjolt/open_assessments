25643eb2d63b584733d19d04c65a967e41e499b5
dspencer001 <dspencer577@gmail.com>
2017-03-17 19:18:58 -0600

    remove default empty string

--- client/js/middleware/serialization/qbank/serializers/base.js
+++ client/js/middleware/serialization/qbank/serializers/base.js
@@ -8,7 +8,7 @@ function serializeQuestionString(originalItem, item) {
   if (!simpleLanguage) {
     return null;
   }
-  let text = _.get(item, 'question.text', '');
+  let text = _.get(item, 'question.text');
   if (!_.isEmpty(originalItem.question) && _.isEmpty(text) && text !== '') {
     text = originalItem.question.text;
   }
