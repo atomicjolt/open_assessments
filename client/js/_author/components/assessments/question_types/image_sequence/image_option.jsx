60c112d489810b56df12094af39ecc7d8c74ceeb
Benjamin Call <llacneb@gmail.com>
2017-04-03 17:42:59 -0600

    fixes locales stff

--- client/js/_author/components/assessments/question_types/image_sequence/image_option.jsx
+++ client/js/_author/components/assessments/question_types/image_sequence/image_option.jsx
@@ -4,7 +4,7 @@ import localize     from '../../../../locales/localize';
 
 function imageOption(props) {
   const { activateChoice, updateChoice, deleteChoice, id, order, numChoices } = props;
-  const strings = this.props.localizeStrings();
+  const strings = props.localizeStrings();
   return (
     <div
       className='au-c-image-sequence-answer is-active'
