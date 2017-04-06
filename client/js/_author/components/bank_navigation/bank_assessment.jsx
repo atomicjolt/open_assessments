2b35e62c4dd29a07e99c06b1ed449f9be87b449a
dittonjs <jditton.atomic@gmail.com>
2017-04-06 13:23:50 -0600

    long line

--- client/js/_author/components/bank_navigation/bank_assessment.jsx
+++ client/js/_author/components/bank_navigation/bank_assessment.jsx
@@ -59,6 +59,7 @@ export default class BankAssessment extends React.Component {
         </ListItem>
       );
     }
+    //a;lsdkjfa;lskdjf;laksjdf;lkajsd;lfkja;sldkfjas;dkjf;laksjdf;lkajsd;lfkjas;ldkjf;laskdjf;laksjdf;lkasjdf;lkajsd;lfkjas;dlkfjas;ldkfja;lskdjf;laskdjf;laksjdf;lkasjdf;lkasjd;lfkjas
     return (
       <ListItem
         {...this.props}
