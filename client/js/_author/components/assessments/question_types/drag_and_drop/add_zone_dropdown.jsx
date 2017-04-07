6cb96ccbae040c2c3f338c1d7ab494726c101908
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2017-04-07 15:41:56 -0600

    spec additions to drag and drop code

--- client/js/_author/components/assessments/question_types/drag_and_drop/add_zone_dropdown.jsx
+++ client/js/_author/components/assessments/question_types/drag_and_drop/add_zone_dropdown.jsx
@@ -14,6 +14,7 @@ export default function addZoneDropdown(props) {
         <ul className={`au-c-button-dropdown__dropdown ${props.active ? 'is-active' : ''}`}>
           <li>
             <button
+              id={`reg_${props.text}`}
               className="au-c-btn au-c-btn--sm au-c-btn--dropdown-item"
               onClick={props.addByRegion}
             >
@@ -23,6 +24,7 @@ export default function addZoneDropdown(props) {
           </li>
           <li>
             <button
+              id={`image_${props.text}`}
               className="au-c-btn au-c-btn--sm au-c-btn--dropdown-item"
               onClick={props.addByImage}
             >
