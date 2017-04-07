b92fdfe96f39c04ff863285593e450cc6e71bad4
dspencer001 <dspencer577@gmail.com>
2017-04-06 19:54:31 -0600

    Describes were unnecessary and would make the message more confusing

--- client/js/middleware/serialization/serializer_utils.spec.js
+++ client/js/middleware/serialization/serializer_utils.spec.js
@@ -2,27 +2,23 @@ import _          from 'lodash';
 import { scrub }  from './serializer_utils';
 
 describe('scrub', () => {
-  describe('arrays', () => {
-    it('returns an array when it is passed an array', () => {
-      expect(_.isArray(scrub([]))).toBeTruthy();
-    });
-
-    it('removes falsey and empty values', () => {
-      expect(scrub([null, undefined, {}, []])).toEqual([]);
-    });
+  it('returns an array when it is passed an array', () => {
+    expect(_.isArray(scrub([]))).toBeTruthy();
   });
 
-  describe('objects', () => {
-    it('returns an object when it is passed an array', () => {
-      expect(_.isPlainObject(scrub({}))).toBeTruthy();
-    });
+  it('removes falsey and empty values from arrays', () => {
+    expect(scrub([null, undefined, {}, []])).toEqual([]);
+  });
 
-    it('removes falsey and empty values', () => {
-      expect(scrub({ a: null, b: undefined, c: {}, d: [] })).toEqual({});
-    });
+  it('returns an object when it is passed an object', () => {
+    expect(_.isPlainObject(scrub({}))).toBeTruthy();
+  });
 
-    it('excludes specified keys', () => {
-      expect(scrub({ a: null, b: undefined, c: {}, d: [] }, ['a'])).toEqual({ a: null });
-    });
+  it('removes falsey and empty values from objects', () => {
+    expect(scrub({ a: null, b: undefined, c: {}, d: [] })).toEqual({});
+  });
+
+  it('excludes specified keys from objects', () => {
+    expect(scrub({ a: null, b: undefined, c: {}, d: [] }, ['a'])).toEqual({ a: null });
   });
 });
