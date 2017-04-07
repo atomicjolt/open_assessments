42748faf65a3a16f33957c47208e985fd9f8fe39
dspencer001 <dspencer577@gmail.com>
2017-04-06 19:47:38 -0600

    Fix scrubber and write a spec for it

---
+++ client/js/middleware/serialization/serializer_utils.spec.js
@@ -0,0 +1,28 @@
+import _          from 'lodash';
+import { scrub }  from './serializer_utils';
+
+describe('scrub', () => {
+  describe('arrays', () => {
+    it('returns an array when it is passed an array', () => {
+      expect(_.isArray(scrub([]))).toBeTruthy();
+    });
+
+    it('removes falsey and empty values', () => {
+      expect(scrub([null, undefined, {}, []])).toEqual([]);
+    });
+  })
+
+  describe('objects', () => {
+    it('returns an object when it is passed an array', () => {
+      expect(_.isPlainObject(scrub({}))).toBeTruthy();
+    });
+
+    it('removes falsey and empty values', () => {
+      expect(scrub({ a: null, b: undefined, c: {}, d: [] })).toEqual({});
+    });
+
+    it('excludes specified keys', () => {
+      expect(scrub({ a: null, b: undefined, c: {}, d: [] }, ['a'])).toEqual({ a: null });
+    });
+  });
+});
