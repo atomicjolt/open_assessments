bc407e21c17edbd67d0d9f30292991bd5f3daa64
cjshawMIT <cjshaw@mit.edu>
2017-04-11 11:48:31 -0400

    prefer server setting for api_url over url param (#37)
    * prefer server setting for api_url over url param
    
    * fix test string grammar

--- client/js/_player/reducers/settings.spec.js
+++ client/js/_player/reducers/settings.spec.js
@@ -29,5 +29,33 @@ describe('settings reducer', () => {
       const settings = getInitialSettings(serverSettings);
       expect(settings.questions_per_page).toEqual(1);
     });
+
+    it("Maintains the server setting for api_url", () => {
+      const serverSettings1 = { api_url: "localhost" };
+      const serverSettings2 = { api_url: "www.example.com" };
+      const settings = getInitialSettings(serverSettings1, serverSettings2);
+      expect(settings.api_url).toEqual("localhost");
+    });
+
+    it("Uses query params for api_url if server settings is empty string", () => {
+      const serverSettings1 = { api_url: "" };
+      const serverSettings2 = { api_url: "www.example.com" };
+      const settings = getInitialSettings(serverSettings1, serverSettings2);
+      expect(settings.api_url).toEqual("www.example.com");
+    });
+
+    it("Uses query params for api_url if server settings is null", () => {
+      const serverSettings1 = { api_url: null };
+      const serverSettings2 = { api_url: "www.example.com" };
+      const settings = getInitialSettings(serverSettings1, serverSettings2);
+      expect(settings.api_url).toEqual("www.example.com");
+    });
+
+    it("Uses query params for api_url if server settings is not present", () => {
+      const serverSettings1 = { foo: "bar" };
+      const serverSettings2 = { api_url: "www.example.com" };
+      const settings = getInitialSettings(serverSettings1, serverSettings2);
+      expect(settings.api_url).toEqual("www.example.com");
+    });
   });
 });
