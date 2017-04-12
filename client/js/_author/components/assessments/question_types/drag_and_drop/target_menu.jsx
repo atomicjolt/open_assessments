5ee2ce6787aec1d85d89a53cd499aff48e055fdc
Ben Smith <ben.smith@atomicjolt.com>
2017-04-12 14:53:26 -0600

    Dragon placeholder (#363)
    * Just moving a bunch of stuff
    
    * image placeholder
    
    * Brandon did this I swear
    
    * I made this a function now
    
    * adds localization

--- client/js/_author/components/assessments/question_types/drag_and_drop/target_menu.jsx
+++ client/js/_author/components/assessments/question_types/drag_and_drop/target_menu.jsx
@@ -1,106 +1,48 @@
 import React      from 'react';
 import AddZone    from './add_zone_dropdown';
-import MediaModal from '../../../common/upload_modal/editor_upload_modal';
-import localize           from '../../../../locales/localize';
+import localize   from '../../../../locales/localize';
 
-export class TargetMenu extends React.Component {
-  static propTypes = {
-    uploadMedia: React.PropTypes.func.isRequired,
-    newZone: React.PropTypes.func.isRequired,
-    localizeStrings: React.PropTypes.func.isRequired,
-    images: React.PropTypes.shape({}).isRequired,
-    loadingMedia: React.PropTypes.bool,
-    hasTarget: React.PropTypes.bool,
-  };
+export function targetMenu(props) {
+  const strings = props.localizeStrings('targetMenu');
 
-  constructor() {
-    super();
-    this.state = {
-      add: null,
-      showModal: false,
-    };
-  }
-
-  replaceImage(media, metadata, newMedia) {
-    this.props.uploadMedia(media, 'target', metadata, newMedia);
-  }
-
-  selectImage(media, metadata, newMedia) {
-    const type = this.state.add;
-    if (type === 'snap' || type === 'drop') {
-      const img = new Image();
-      img.src = URL.createObjectURL(media);
-      img.onload = () => {
-        // TODO: make sure image isn't bigger than the area
-        this.props.newZone({
-          type,
-          height: img.height,
-          width: img.width,
-        });
-      };
-      this.props.uploadMedia(media, 'dropObjects.new', metadata, newMedia);
-    } else {
-      this.replaceImage(media, metadata, newMedia);
-    }
-    this.setState({
-      showModal: false,
-      add: null
-    });
-  }
-
-  addByRegion() {
-    this.props.newZone({
-      type: this.state.add,
-    });
-    this.setState({ add: null });
-  }
-
-  addByImage() {
-    this.setState({ showModal: true });
-  }
-
-  render() {
-    const strings = this.props.localizeStrings('targetMenu');
-
-    return (
-      <div className="au-o-item__top">
-        <div className="au-o-left">
-          <div className="au-c-question__type">{strings.targetImage}</div>
-        </div>
-        <div className="au-o-right">
-          <button
-            className="au-c-btn au-c-btn--sm au-c-btn--gray"
-            onClick={() => this.setState({ showModal: true })}
-          >
-            {this.props.hasTarget ? strings.replace : strings.addImage}
-          </button>
-          <AddZone
-            active={this.state.add === 'snap'}
-            text={strings.addSnap}
-            toggle={() => this.setState({ add: this.state.add !== 'snap' ? 'snap' : null })}
-            addByRegion={() => this.addByRegion()}
-            addByImage={() => this.addByImage()}
-          />
-          <AddZone
-            active={this.state.add === 'drop'}
-            text={strings.addDrop}
-            toggle={() => this.setState({ add: this.state.add !== 'drop' ? 'drop' : null })}
-            addByRegion={() => this.addByRegion()}
-            addByImage={() => this.addByImage()}
-          />
-        </div>
-        <MediaModal
-          isOpen={this.state.showModal}
-          closeModal={() => this.setState({ showModal: false })}
-          loading={this.props.loadingMedia}
-          media={this.props.images}
-          mediaType={'img'}
-          insertMedia={(media, metadata, newMedia) => this.selectImage(media, metadata, newMedia)}
-          uploadOnly={!!this.state.add}
+  return (
+    <div className="au-o-item__top">
+      <div className="au-o-left">
+        <div className="au-c-question__type">{strings.targetImage}</div>
+      </div>
+      <div className="au-o-right">
+        <button
+          className="au-c-btn au-c-btn--sm au-c-btn--gray"
+          onClick={props.openModal}
+        >
+          {props.hasTarget ? strings.replace : strings.addImage}
+        </button>
+        <AddZone
+          active={props.addType === 'snap'}
+          text={strings.addSnap}
+          toggle={() => props.toggleAdd('snap')}
+          addByRegion={props.addByRegion}
+          addByImage={props.openModal}
+        />
+        <AddZone
+          active={props.addType === 'drop'}
+          text={strings.addDrop}
+          toggle={() => props.toggleAdd('drop')}
+          addByRegion={props.addByRegion}
+          addByImage={props.openModal}
         />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default localize(TargetMenu);
+targetMenu.propTypes = {
+  localizeStrings: React.PropTypes.func.isRequired,
+  openModal: React.PropTypes.func.isRequired,
+  toggleAdd: React.PropTypes.func.isRequired,
+  addByRegion: React.PropTypes.func.isRequired,
+  hasTarget: React.PropTypes.bool,
+  addType: React.PropTypes.string,
+};
+
+export default localize(targetMenu);
