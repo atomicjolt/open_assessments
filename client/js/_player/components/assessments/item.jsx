d14a9cbe41e5ff806cdf7b6f10133a0826ed6c79
cjshaw <cjshaw@mit.edu>
2017-04-13 09:19:43 -0400

    remove redundant imports

--- client/js/_player/components/assessments/item.jsx
+++ client/js/_player/components/assessments/item.jsx
@@ -2,8 +2,6 @@
 
 import React  from "react";
 import _                       from 'lodash';
-import * as AssessmentActions  from "../../actions/assessment";
-import UniversalInput          from "./universal_input";
 import videojs from 'video.js';
 import * as AssessmentActions  from '../../actions/assessment';
 import UniversalInput          from './universal_input';
