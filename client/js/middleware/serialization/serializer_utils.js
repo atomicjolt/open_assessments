71cf9d7cdb4a44dd2eab950bc51936206548e495
dspencer001 <dspencer577@gmail.com>
2017-04-17 13:31:07 -0600

    Make feedback for all languages work

--- client/js/middleware/serialization/serializer_utils.js
+++ client/js/middleware/serialization/serializer_utils.js
@@ -18,7 +18,7 @@ export function scrub(item, protectedKeys) {
 }
 
 export function languageText(text, language) {
-  if (!text) return null;
+  if (!text) return '';
   return {
     text,
     languageTypeId: language,
