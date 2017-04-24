0c4bd2e7d440eed2265fce9b6e5424a216fc95e1
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2017-04-24 13:00:16 -0600

    image sequence spec

---
+++ client/js/middleware/serialization/qbank/serializers/image_sequence.spec.js
@@ -0,0 +1,52 @@
+import ImageSequence          from './image_sequence';
+import languages              from '../../../../constants/language_types';
+import genusTypes             from '../../../../constants/genus_types';
+
+describe('image_sequence serialization', () => {
+  let item;
+  let newItemAttr;
+  let result;
+
+  beforeEach(() => {
+    item = {
+      question: {
+        correctFeedback: { answerId: '1' },
+        choices: {},
+      },
+      answers: [{
+        feedback: { text: '<p>Howdy</p>' },
+        genusTypeId: genusTypes.answer.rightAnswer,
+        fileIds: {},
+        choiceIds: []
+      }, {
+        feedback: { text: '<p>hiya</p>' },
+        genusTypeId: genusTypes.answer.wrongAnswer,
+        fileIds: {}
+      }],
+    };
+    newItemAttr = {
+      question: {
+        choices: [{},
+        {}],
+        correctFeedback: {
+          text: 'correctText',
+          fileIds: {},
+          id: '1',
+        },
+      },
+      language: languages.languageTypeId.english,
+    };
+    result = ImageSequence(item, newItemAttr);
+  });
+
+  it('handles the serialization', () => {
+    expect(result.answers[0].feedback.text).toBe('correctText');
+  });
+
+  it('skips the answers when proper data not available', () => {
+    newItemAttr.question.choices = null;
+    newItemAttr.question.correctFeedback = null;
+    result = ImageSequence(item, newItemAttr);
+    expect(result.answers).not.toBeDefined();
+  });
+});
