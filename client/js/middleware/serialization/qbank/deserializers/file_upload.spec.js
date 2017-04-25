78ccce73854ff32d9a80d7714744853409906159
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2017-04-25 13:52:52 -0600

    final serializer specs

---
+++ client/js/middleware/serialization/qbank/deserializers/file_upload.spec.js
@@ -0,0 +1,27 @@
+import FileUpload    from './file_upload';
+
+describe('file_upload deserializer', () => {
+  it('deserializes the question', () => {
+    const item = {
+      id: 7,
+      question: {
+        correctFeedback: 'Spec Feedback',
+        timeValue: {
+          hours: 1,
+          minutes: 70,
+          seconds: 100,
+        },
+      },
+      answers: [{
+        feedback: {
+          text: 'something witty',
+          texts: [],
+        },
+        id: '7',
+        fileIds: {},
+      }],
+    };
+    const result = FileUpload(item);
+    expect(result.question.correctFeedback.text).toEqual('something witty');
+  });
+});
