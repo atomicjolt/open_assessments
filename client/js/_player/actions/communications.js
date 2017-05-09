ce0d297001b4f37bb0da8fe52ecf0c1d7dd6568c
Justin Ball <justinball@gmail.com>
2017-05-09 16:58:32 -0600

    Restore communicator

--- client/js/_player/actions/communications.js
+++ client/js/_player/actions/communications.js
@@ -8,57 +8,18 @@ const constants = [
   'LTI_NAVIGATE_PREVIOUS',
   'HIDE_LMS_NAVIGATION',
   'SHOW_LMS_NAVIGATION',
-  'AVAILABLE_LOCALES'
+  'AVAILABLE_LOCALES',
 ];
 
 const requests = [];
 
 export const Constants = wrapper(constants, requests);
 
-export const sendSize = () => {
-  return {
-    type: Constants.SEND_SIZE
-  };
-};
-
-export const scrollParentToTop = () => {
-  return {
-    type: Constants.SCROLL_PARENT_TO_TOP
-  };
-};
-
-export const navigateHome = () => {
-  return {
-    type: Constants.LTI_NAVIGATE_HOME
-  };
-};
-
-export const navigateNext = () => {
-  return {
-    type: Constants.LTI_NAVIGATE_NEXT
-  };
-};
-
-export const navigatePrevious = () => {
-  return {
-    type: Constants.LTI_NAVIGATE_PREVIOUS
-  };
-};
-
-export const hideLMSNavigation = () => {
-  return {
-    type: Constants.HIDE_LMS_NAVIGATION
-  };
-};
-
-export const showLMSNavigation = () => {
-  return {
-    type: Constants.SHOW_LMS_NAVIGATION
-  };
-};
-
-export const availableLocales = () => {
-  return {
-    type: Constants.AVAILABLE_LOCALES
-  };
-};
+// TODO: what do any of these do?
+export const scrollParentToTop = () => ({ type: Constants.SCROLL_PARENT_TO_TOP });
+export const navigateHome = () => ({ type: Constants.LTI_NAVIGATE_HOME });
+export const navigateNext = () => ({ type: Constants.LTI_NAVIGATE_NEXT });
+export const navigatePrevious = () => ({ type: Constants.LTI_NAVIGATE_PREVIOUS });
+export const hideLMSNavigation = () => ({ type: Constants.HIDE_LMS_NAVIGATION });
+export const showLMSNavigation = () => ({ type: Constants.SHOW_LMS_NAVIGATION });
+export const availableLocales = () => ({ type: Constants.AVAILABLE_LOCALES });
